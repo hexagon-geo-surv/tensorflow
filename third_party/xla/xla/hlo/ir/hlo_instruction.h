2b18503a30daff68e080b492a78c913791a3d604
A. Unique TensorFlower <gardener@tensorflow.org>
2024-04-18 15:08:03 -0700

    [Cleanup] Remove unused CreateCollectiveBroadcast function.
    PiperOrigin-RevId: 626162285

--- third_party/xla/xla/hlo/ir/hlo_instruction.h
+++ third_party/xla/xla/hlo/ir/hlo_instruction.h
@@ -963,12 +963,6 @@ class HloInstruction {
       absl::Span<const ReplicaGroup> replica_groups, bool constrain_layout,
       const std::optional<int64_t>& channel_id);
 
-  static std::unique_ptr<HloInstruction> CreateCollectiveBroadcast(
-      const Shape& shape, HloInstruction* input, HloInstruction* output,
-      HloInstruction* input_start_indices, HloInstruction* output_start_indices,
-      absl::Span<const ReplicaGroup> replica_groups, bool constrain_layout,
-      const std::optional<int64_t>& channel_id);
-
   // Creates a communication instruction that permutes data cross replicas.
   // Data is sent/received according to the (source_replica_id,
   // target_replica_id) pairs in `source_target_pairs`. If a replica id is not a
