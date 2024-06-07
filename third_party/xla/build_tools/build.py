7a900664e19977a0f635b2f2a00c5f88d7be85f1
David Dunleavy <ddunleavy@google.com>
2024-06-07 14:41:04 -0700

    Refactor in preparation for moving JAX/XLA CI to build.py
    * Move some `DockerImage` fields to arguments to `pull_and_run`
    * Don't supply a default workdir
    * Don't give a default for `target_patterns` in BUILD
    * Rename `_DEFAULT_TARGET_PATTERNS` to `_XLA_DEFAULT_TARGET_PATTERNS`
    * Move comment about cudnn9 container to a more appropriate location
    
    Reverts changelist 641225077
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/13523 from ROCm:ci_hotfit_20240607 931ddf1d5b87c85c3fe1d825665015882d50ef42
    PiperOrigin-RevId: 641362115

--- third_party/xla/build_tools/build.py
+++ third_party/xla/build_tools/build.py
@@ -23,6 +23,7 @@ assumed are:
 The script also assumes that the working directory never changes modulo `cd`ing
 into the repo that should be built (mostly `github/xla`, but also JAX and TF).
 """
+import contextlib
 import dataclasses
 import enum
 import logging
@@ -30,7 +31,7 @@ import os
 import subprocess
 import sys
 import time
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 
 _KW_ONLY_IF_PYTHON310 = {"kw_only": True} if sys.version_info >= (3, 10) else {}
 
@@ -45,7 +46,34 @@ _DEFAULT_BAZEL_OPTIONS = dict(
     jobs=150,
 )
 
-_DEFAULT_TARGET_PATTERNS = ("//xla/...", "//build_tools/...", "@local_tsl//tsl/...")
+_DEFAULT_DOCKER_OPTIONS = dict(
+    rm=True,
+    interactive=True,
+    detach=True,
+    tty=True,
+    volume="./github:/github",
+)
+
+_XLA_DEFAULT_TARGET_PATTERNS = (
+    "//xla/...",
+    "//build_tools/...",
+    "@local_tsl//tsl/...",
+)
+
+
+def sh(args, check=True, **kwargs):
+  logging.info("Starting process: %s", " ".join(args))
+  return subprocess.run(args, check=check, **kwargs)
+
+
+def _dict_to_cli_options(d: Dict[str, Any]) -> List[str]:
+  # pylint: disable=g-bool-id-comparison
+  return [f"--{k}" if v is True else f"--{k}={v}" for k, v in d.items()]
+
+
+def _write_to_sponge_config(key, value) -> None:
+  with open("custom_sponge_config.csv", "a") as f:
+    f.write(f"{key},{value}\n")
 
 
 class BuildType(enum.Enum):
@@ -56,22 +84,82 @@ class BuildType(enum.Enum):
 
 
 @dataclasses.dataclass(frozen=True, **_KW_ONLY_IF_PYTHON310)
+class DockerImage:
+  """Class representing a docker image."""
+
+  image_url: str
+
+  def _pull_docker_image_with_retries(self, retries=3) -> None:
+    """Pulls docker image with retries to avoid transient rate limit errors."""
+    for _ in range(retries):
+      pull_proc = sh(["docker", "pull", self.image_url], check=False)
+      if pull_proc.returncode != 0:
+        time.sleep(15)
+
+    # write SHA of image to the sponge config
+    _write_to_sponge_config("TF_INFO_DOCKER_IMAGE", self.image_url)
+
+    _ = sh(["docker", "pull", self.image_url])
+    # TODO(ddunleavy): get sha
+    # _write_to_sponge_config("TF_INFO_DOCKER_SHA", sha)
+
+  @contextlib.contextmanager
+  def pull_and_run(
+      self,
+      name: str = "xla_ci",
+      command: Tuple[str, ...] = ("bash",),
+      **kwargs: Any,
+  ):
+    """Context manager for the container that yields `docker exec` lambda.
+
+    Args:
+      name: The name of the docker container.
+      command: Command given to `docker run`, e.g. `bash`
+      **kwargs: Extra options passed to `docker run`.
+
+    Yields:
+      A function that accepts a command as a list of args, and runs those on the
+      corresponding docker container. It shouldn't be used outside the `with`
+      block, as the container will be stopped after the end of the block.
+
+    This manages pulling, starting, and stopping the container. Example usage:
+    ```
+    with image.pull_and_run() as docker_exec:
+      docker_exec(["command", "--with", "--flags"])
+    ```
+    """
+    try:
+      self._pull_docker_image_with_retries()
+      options = _dict_to_cli_options(kwargs)
+      sh([
+          "docker",
+          "run",
+          "--name",
+          name,
+          *options,
+          self.image_url,
+          *command,
+      ])
+      docker_exec = lambda args: sh(["docker", "exec", name, *args])
+      yield docker_exec
+    finally:
+      sh(["docker", "stop", name])
+
+
+@dataclasses.dataclass(frozen=True, **_KW_ONLY_IF_PYTHON310)
 class Build:
   """Class representing a build of XLA."""
 
   type_: BuildType
   repo: str
+  docker_image: DockerImage
+  target_patterns: Tuple[str, ...]
   configs: Tuple[str, ...] = ()
-  target_patterns: Tuple[str, ...] = _DEFAULT_TARGET_PATTERNS
   tag_filters: Tuple[str, ...] = ()
   options: Dict[str, Any] = dataclasses.field(default_factory=dict)
-  docker_image: Optional[str] = None
 
   def bazel_test_command(self) -> List[str]:
-    # pylint: disable=g-bool-id-comparison
-    options = [
-        f"--{k}" if v is True else f"--{k}={v}" for k, v in self.options.items()
-    ]
+    options = _dict_to_cli_options(self.options)
     configs = [f"--config={config}" for config in self.configs]
     build_tag_filters = f"--build_tag_filters={','.join(self.tag_filters)}"
     test_tag_filters = f"--test_tag_filters={','.join(self.tag_filters)}"
@@ -92,6 +180,20 @@ def _tag_filters_for_compute_capability(
   return tag_filters
 
 
+_DEFAULT_IMAGE = DockerImage(
+    image_url="gcr.io/tensorflow-sigs/build:latest-python3.11",
+)
+
+# TODO(b/338885148): Remove this once the TF containers have cuDNN 9
+_CUDNN_9_IMAGE = DockerImage(
+    image_url="gcr.io/tensorflow-sigs/build@sha256:dddcaf30321e9007103dce75c51b83fea3c06de462fcf41e7c6ae93f37fc3545",
+)
+
+_ARM64_JAX_MULTI_PYTHON_IMAGE = DockerImage(
+    image_url="us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/build-arm64:jax-latest-multi-python",
+)
+
+
 def nvidia_gpu_build_with_compute_capability(
     *, type_: BuildType, compute_capability: int
 ) -> Build:
@@ -99,22 +201,22 @@ def nvidia_gpu_build_with_compute_capability(
   return Build(
       type_=type_,
       repo="xla",
+      docker_image=_CUDNN_9_IMAGE,
+      target_patterns=_XLA_DEFAULT_TARGET_PATTERNS,
       configs=("warnings", "rbe_linux_cuda_nvcc"),
       tag_filters=("-no_oss", "requires-gpu-nvidia") + extra_gpu_tags,
       options=dict(
           run_under="//tools/ci_build/gpu_build:parallel_gpu_execute",
           **_DEFAULT_BAZEL_OPTIONS,
       ),
-      # TODO(b/338885148): Remove this once the TF containers have cuDNN 9
-      docker_image="gcr.io/tensorflow-sigs/build@sha256:dddcaf30321e9007103dce75c51b83fea3c06de462fcf41e7c6ae93f37fc3545",
   )
-
 
 _CPU_X86_BUILD = Build(
     type_=BuildType.CPU_X86,
     repo="xla",
+    docker_image=_DEFAULT_IMAGE,
     configs=("warnings", "nonccl", "rbe_linux_cpu"),
-    target_patterns=_DEFAULT_TARGET_PATTERNS + ("-//xla/service/gpu/...",),
+    target_patterns=_XLA_DEFAULT_TARGET_PATTERNS + ("-//xla/service/gpu/...",),
     tag_filters=(
         "-no_oss",
         "-gpu",
@@ -122,13 +224,13 @@ _CPU_X86_BUILD = Build(
         "-requires-gpu-amd",
     ),
     options=_DEFAULT_BAZEL_OPTIONS,
-    docker_image="gcr.io/tensorflow-sigs/build:latest-python3.11",
 )
 _CPU_ARM64_BUILD = Build(
     type_=BuildType.CPU_ARM64,
     repo="xla",
+    docker_image=_ARM64_JAX_MULTI_PYTHON_IMAGE,
     configs=("warnings", "rbe_cross_compile_linux_arm64_xla", "nonccl"),
-    target_patterns=_DEFAULT_TARGET_PATTERNS + ("-//xla/service/gpu/...",),
+    target_patterns=_XLA_DEFAULT_TARGET_PATTERNS + ("-//xla/service/gpu/...",),
     tag_filters=(
         "-no_oss",
         "-gpu",
@@ -137,7 +239,6 @@ _CPU_ARM64_BUILD = Build(
         "-requires-gpu-amd",
     ),
     options={**_DEFAULT_BAZEL_OPTIONS, "build_tests_only": True},
-    docker_image="us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/build-arm64:jax-latest-multi-python",
 )
 _GPU_BUILD = nvidia_gpu_build_with_compute_capability(
     type_=BuildType.GPU, compute_capability=75
@@ -156,31 +257,6 @@ _KOKORO_JOB_NAME_TO_BUILD_MAP = {
 }
 
 
-def _write_to_sponge_config(key, value) -> None:
-  with open("custom_sponge_config.csv", "a") as f:
-    f.write(f"{key},{value}\n")
-
-
-def sh(args, check=True, **kwargs):
-  logging.info("Starting process: %s", " ".join(args))
-  return subprocess.run(args, check=check, **kwargs)
-
-
-def _pull_docker_image_with_retries(image_url: str, retries=3) -> None:
-  """Pulls docker image with retries to avoid transient rate limit errors."""
-  for _ in range(retries):
-    pull_proc = sh(["docker", "pull", image_url], check=False)
-    if pull_proc.returncode != 0:
-      time.sleep(15)
-
-  # write SHA of image to the sponge config
-  _write_to_sponge_config("TF_INFO_DOCKER_IMAGE", image_url)
-
-  pull_proc = sh(["docker", "pull", image_url], capture_output=True, text=True)
-  # TODO(ddunleavy): get sha
-  # _write_to_sponge_config("TF_INFO_DOCKER_SHA", sha)
-
-
 def main():
   logging.basicConfig()
   logging.getLogger().setLevel(logging.INFO)
@@ -188,23 +264,6 @@ def main():
   build = _KOKORO_JOB_NAME_TO_BUILD_MAP[kokoro_job_name]
 
   sh(["./github/xla/.kokoro/generate_index_html.sh", "index.html"])
-
-  _pull_docker_image_with_retries(build.docker_image)
-
-  sh(
-      # pyformat: disable
-      [
-          "docker", "run",
-          "--name", "xla",
-          "-w", f"/github/{build.repo}",
-          "-itd",
-          "--rm",
-          "-v", "./github:/github",
-          build.docker_image,
-          "bash",
-      ],
-      # pyformat: enable
-  )
 
   # TODO(b/338885148): Remove this block after TF was updated to cuDNN 9
   if build.type_ in (BuildType.GPU, BuildType.GPU_CONTINUOUS):
@@ -217,11 +276,11 @@ def main():
         ],
     )
 
-  sh(["docker", "exec", "xla", *build.bazel_test_command()])
-
-  sh(["docker", "exec", "xla", "bazel", "analyze-profile", "profile.json.gz"])
-
-  sh(["docker", "stop", "xla"])
+  with build.docker_image.pull_and_run(
+      workdir=f"/github/{build.repo}", **_DEFAULT_DOCKER_OPTIONS
+  ) as docker_exec:
+    docker_exec(build.bazel_test_command())
+    docker_exec(["bazel", "analyze-profile", "profile.json.gz"])
 
 
 if __name__ == "__main__":
