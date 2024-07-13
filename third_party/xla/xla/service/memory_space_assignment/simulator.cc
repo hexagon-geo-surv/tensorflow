031987f379c06e2a19de0cd7efd97feccc9330e2
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-12 17:52:33 -0700

    [XLA:MSA] Support slice instruction in runtime simulator
    This patch includes the slice instruction overhead into the runtime simulator. The slice instruction introduces async transfer between default<->alternate memory space, which is the same as the copy-start copy-done instruction, except the slice instructions have different way to calculate the transfer bytes.
    
    PiperOrigin-RevId: 651933615

--- third_party/xla/xla/service/memory_space_assignment/simulator.cc
+++ third_party/xla/xla/service/memory_space_assignment/simulator.cc
@@ -15,15 +15,23 @@ limitations under the License.
 
 #include "xla/service/memory_space_assignment/simulator.h"
 
+#include <algorithm>
 #include <cstdint>
+#include <list>
 #include <memory>
+#include <optional>
 #include <utility>
-#include <vector>
 
+#include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
-#include "xla/hlo/ir/hlo_instruction.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/types/span.h"
+#include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/hlo/ir/hlo_schedule.h"
 #include "xla/hlo/utils/hlo_live_range.h"
+#include "xla/layout.h"
 #include "xla/service/hlo_value.h"
 #include "xla/service/memory_space_assignment/allocation.h"
 #include "xla/shape_util.h"
@@ -32,30 +40,30 @@ limitations under the License.
 namespace xla {
 namespace memory_space_assignment {
 
-float RuntimeSimulator::ComputeEstimatedElapsedTime(
-    const HloLiveRange& hlo_live_range, const AllocationSequence& allocations) {
-  absl::flat_hash_map<const HloInstruction*, std::vector<ShapeIndex>>
-      outputs_in_alternate_memory_map;
-  absl::flat_hash_map<const HloInstruction*,
-                      std::vector<std::pair<int64_t, ShapeIndex>>>
-      operands_in_alternate_memory_map;
-
+void RuntimeSimulator::InitializeAlternateMemoryMap(
+    const AllocationSequence& allocations) {
+  outputs_in_alternate_memory_map_.clear();
+  operands_in_alternate_memory_map_.clear();
   for (auto& allocation : allocations) {
     if (!allocation->is_copy_allocation()) {
       if (allocation->memory_space() == MemorySpace::kAlternate) {
         const HloInstruction* defining_instruction =
             allocation->defining_position().instruction;
-        outputs_in_alternate_memory_map[defining_instruction].push_back(
+        outputs_in_alternate_memory_map_[defining_instruction].push_back(
             allocation->defining_position().index);
       }
     }
     for (auto& hlo_use : allocation->uses()) {
       const HloInstruction* use_instruction = hlo_use.instruction;
-      operands_in_alternate_memory_map[use_instruction].push_back(
+      operands_in_alternate_memory_map_[use_instruction].push_back(
           std::make_pair(hlo_use.operand_number, hlo_use.operand_index));
     }
   }
+}
 
+float RuntimeSimulator::SimulateElapsedTimeWithoutAsyncCopies(
+    const HloLiveRange& hlo_live_range, const AllocationSequence& allocations) {
+  InitializeAlternateMemoryMap(allocations);
   const auto& instruction_sequence =
       hlo_live_range.flattened_instruction_sequence().instructions();
   float total_elapsed = 0.0;
@@ -63,15 +71,18 @@ float RuntimeSimulator::ComputeEstimatedElapsedTime(
     if (instruction->opcode() == HloOpcode::kWhile) {
       continue;
     }
-    std::vector<ShapeIndex> outputs_in_alternate_memory;
-    auto output_it = outputs_in_alternate_memory_map.find(instruction);
-    if (output_it != outputs_in_alternate_memory_map.end()) {
-      outputs_in_alternate_memory = output_it->second;
+
+    absl::Span<const ShapeIndex> outputs_in_alternate_memory;
+    auto output_it = outputs_in_alternate_memory_map_.find(instruction);
+    if (output_it != outputs_in_alternate_memory_map_.end()) {
+      outputs_in_alternate_memory = absl::MakeSpan(output_it->second);
     }
-    std::vector<std::pair<int64_t, ShapeIndex>> operands_in_alternate_memory;
-    auto operand_it = operands_in_alternate_memory_map.find(instruction);
-    if (operand_it != operands_in_alternate_memory_map.end()) {
-      operands_in_alternate_memory = operand_it->second;
+
+    absl::Span<const std::pair<int64_t, ShapeIndex>>
+        operands_in_alternate_memory;
+    auto operand_it = operands_in_alternate_memory_map_.find(instruction);
+    if (operand_it != operands_in_alternate_memory_map_.end()) {
+      operands_in_alternate_memory = absl::MakeSpan(operand_it->second);
     }
 
     float instruction_elapsed_per_invoke =
@@ -83,6 +94,273 @@ float RuntimeSimulator::ComputeEstimatedElapsedTime(
     // Calculate total elapsed time by summing up the overall elapsed time of
     // each instruction.
     total_elapsed += total_trip_count * instruction_elapsed_per_invoke;
+  }
+  return total_elapsed;
+}
+
+MemoryTransferDirection GetAsyncCopyDirection(
+    const HloInstruction* async_copy_start, int64_t alternate_memory_space) {
+  CHECK(async_copy_start->opcode() == HloOpcode::kCopyStart ||
+        (async_copy_start->opcode() == HloOpcode::kAsyncStart &&
+         async_copy_start->async_wrapped_instruction()->opcode() ==
+             HloOpcode::kSlice));
+
+  int64_t operand_memory_space =
+      async_copy_start->operand(0)->shape().layout().memory_space();
+  // Get all users
+  std::optional<int64_t> output_memory_space;
+  for (const HloInstruction* user : async_copy_start->users()) {
+    if (user->opcode() == HloOpcode::kCopyDone ||
+        user->opcode() == HloOpcode::kAsyncDone) {
+      output_memory_space.emplace(user->shape().layout().memory_space());
+      break;
+    }
+  }
+  if (!output_memory_space.has_value()) {
+    return MemoryTransferDirection::kUnsupported;
+  }
+
+  if (operand_memory_space == xla::Layout::kDefaultMemorySpace &&
+      output_memory_space == alternate_memory_space) {
+    return MemoryTransferDirection::kDefaultToAlternate;
+  }
+  if (operand_memory_space == alternate_memory_space &&
+      output_memory_space == xla::Layout::kDefaultMemorySpace) {
+    return MemoryTransferDirection::kAlternateToDefault;
+  }
+  return MemoryTransferDirection::kUnsupported;
+}
+
+const std::list<OutstandingAsyncCopy>&
+RuntimeSimulator::GetOutstandingReadDefaultQueue() const {
+  return outstanding_read_default_queue_;
+}
+
+const std::list<OutstandingAsyncCopy>&
+RuntimeSimulator::GetOutstandingWriteDefaultQueue() const {
+  return outstanding_write_default_queue_;
+}
+
+const HloInstruction* RuntimeSimulator::RemoveBytesFromQueueIfNotEmpty(
+    std::list<OutstandingAsyncCopy>& async_copy_queue, float processed_bytes) {
+  if (async_copy_queue.empty()) return nullptr;
+  CHECK_GE(async_copy_queue.front().remaining_bytes_to_transfer,
+           processed_bytes);
+  async_copy_queue.front().remaining_bytes_to_transfer -= processed_bytes;
+  if (async_copy_queue.front().remaining_bytes_to_transfer == 0.0) {
+    const HloInstruction* retired_instruction =
+        async_copy_queue.front().copy_start_inst;
+    async_copy_queue.pop_front();
+    return retired_instruction;
+  }
+  return nullptr;
+}
+
+float RuntimeSimulator::SimulateAsyncCopyDone(
+    const HloInstruction* copy_done_instruction) {
+  const HloInstruction* copy_start_instruction =
+      copy_done_instruction->operand(0);
+  MemoryTransferDirection direction =
+      GetAsyncCopyDirection(copy_start_instruction, alternate_memory_space_);
+  if (direction == MemoryTransferDirection::kUnsupported) {
+    // The memory access is not a default <-> alternate memory copy.
+    LOG(WARNING) << "Unsupported memory transfer direction for copy-done: "
+                 << copy_done_instruction->ToString();
+    return 0.0;
+  }
+  std::list<OutstandingAsyncCopy>& same_direction_queue =
+      direction == MemoryTransferDirection::kDefaultToAlternate
+          ? outstanding_read_default_queue_
+          : outstanding_write_default_queue_;
+  std::list<OutstandingAsyncCopy>& opposite_direction_queue =
+      direction == MemoryTransferDirection::kDefaultToAlternate
+          ? outstanding_write_default_queue_
+          : outstanding_read_default_queue_;
+
+  if (absl::c_find_if(
+          same_direction_queue, [&](const OutstandingAsyncCopy& async_copy) {
+            return async_copy.copy_start_inst == copy_start_instruction;
+          }) == same_direction_queue.end()) {
+    // The copy has already finished; thus, the copy-done takes no time.
+    return 0.0;
+  }
+
+  // Each iteration of the while loop simulates transferring a number of
+  // bytes from each queue that is equal to the smaller of the two elements
+  // at the front of each queue. If that causes us to finish a copy in the
+  // same_direction_queue, and that copy is the copy_done_instruction, we
+  // break the loop.
+  float elapsed_time = 0.0;
+  const HloInstruction* retired_instruction_in_same_direction_queue = nullptr;
+  // Loop until we process the copy start instruction that the copy-done
+  // instruction is waiting for.
+  do {
+    float bytes_to_process =
+        same_direction_queue.front().remaining_bytes_to_transfer;
+    float available_bandwidth = cost_analysis_->base_costs().BytesPerSecond();
+
+    if (!opposite_direction_queue.empty()) {
+      // Need to share the bandwidth with the opposite direction queue.
+      available_bandwidth *= 0.5;
+      bytes_to_process = std::min(
+          bytes_to_process,
+          opposite_direction_queue.front().remaining_bytes_to_transfer);
+    }
+
+    elapsed_time += bytes_to_process / available_bandwidth;
+
+    RemoveBytesFromQueueIfNotEmpty(opposite_direction_queue, bytes_to_process);
+    retired_instruction_in_same_direction_queue =
+        RemoveBytesFromQueueIfNotEmpty(same_direction_queue, bytes_to_process);
+  } while (retired_instruction_in_same_direction_queue !=
+           copy_start_instruction);
+  return elapsed_time;
+};
+
+float RuntimeSimulator::SimulateComputeInstruction(
+    const HloInstruction* instruction,
+    absl::Span<const std::pair<int64_t, ShapeIndex>>
+        operands_in_alternate_memory,
+    absl::Span<const ShapeIndex> outputs_in_alternate_memory) {
+  // Calculate the time window in which the instruction does not access the
+  // default memory.
+  float default_memory_idle_time =
+      cost_analysis_->GetDefaultMemoryBandwidthIdleTime(
+          *instruction, operands_in_alternate_memory,
+          outputs_in_alternate_memory);
+  // Execute the outstanding async copy in the time window.
+  ProcessAsyncCopiesInTimeWindow(default_memory_idle_time);
+
+  float inst_elapsed = cost_analysis_->GetInstructionElapsedInAlternateMemory(
+      *instruction, operands_in_alternate_memory, outputs_in_alternate_memory);
+  return inst_elapsed;
+}
+
+void RuntimeSimulator::ProcessAsyncCopiesInTimeWindow(float time_window) {
+  if (time_window <= 0) {
+    return;
+  }
+  float elapsed_time_window = time_window;
+  // This loop simulates the execution of the front memory requests in the
+  // read and/or write queues. The loop terminates when the time window is
+  // exhausted or there are no more outstanding async copies.
+  while ((!outstanding_read_default_queue_.empty() ||
+          !outstanding_write_default_queue_.empty()) &&
+         elapsed_time_window > 0) {
+    float available_bandwidth = cost_analysis_->base_costs().BytesPerSecond();
+    if (!outstanding_read_default_queue_.empty() &&
+        !outstanding_write_default_queue_.empty()) {
+      // Need to share the bandwidth
+      available_bandwidth *= 0.5;
+    }
+    float bytes_to_process = available_bandwidth * elapsed_time_window;
+    if (!outstanding_read_default_queue_.empty()) {
+      bytes_to_process = std::min(
+          bytes_to_process,
+          outstanding_read_default_queue_.front().remaining_bytes_to_transfer);
+    }
+    if (!outstanding_write_default_queue_.empty()) {
+      bytes_to_process = std::min(
+          bytes_to_process,
+          outstanding_write_default_queue_.front().remaining_bytes_to_transfer);
+    }
+
+    float real_elapsed_time = bytes_to_process / available_bandwidth;
+    elapsed_time_window -= real_elapsed_time;
+    RemoveBytesFromQueueIfNotEmpty(outstanding_read_default_queue_,
+                                   bytes_to_process);
+    RemoveBytesFromQueueIfNotEmpty(outstanding_write_default_queue_,
+                                   bytes_to_process);
+  }
+}
+
+float RuntimeSimulator::SimulateElapsedTime(
+    const HloModule* hlo_module, const HloLiveRange& hlo_live_range,
+    const AllocationSequence& allocations) {
+  InitializeAlternateMemoryMap(allocations);
+
+  // Cannot provide a valid result if the bandwidth is invalid.
+  if (cost_analysis_->base_costs().BytesPerSecond() <= 0.0) {
+    return 0.0;
+  }
+
+  float total_elapsed = 0.0;
+  for (const HloComputation* computation :
+       hlo_module->MakeNonfusionComputations()) {
+    // We include the overhead of the async copy instructions when computing the
+    // runtime of the caller (copy-done, slice). Thus, we can skip the async
+    // computation as callee.
+    if (computation->IsAsyncComputation()) {
+      continue;
+    }
+    CHECK(
+        hlo_module->has_schedule() &&
+        hlo_module->schedule().sequences().contains(computation->unique_id()));
+
+    const HloInstructionSequence& instruction_sequence =
+        hlo_module->schedule().sequence(computation);
+    for (const HloInstruction* instruction :
+         instruction_sequence.instructions()) {
+      float inst_elapsed = 0.0;
+      if (instruction->opcode() == HloOpcode::kWhile) {
+        // Since the instructions in the while body are calculated
+        // separately, we can skip the while instruction.
+        continue;
+      }
+      if (instruction->opcode() == HloOpcode::kCopyStart ||
+          (instruction->opcode() == HloOpcode::kAsyncStart &&
+           instruction->async_wrapped_instruction()->opcode() ==
+               HloOpcode::kSlice)) {
+        // Try to categorize the async copy instruction into
+        // read-from-default and write-to-default queues.
+        MemoryTransferDirection direction =
+            GetAsyncCopyDirection(instruction, alternate_memory_space_);
+        const Shape& transfer_shape =
+            (instruction->opcode() == HloOpcode::kCopyStart)
+                ? instruction->operand(0)->shape()
+                : ShapeUtil::GetSubshape(instruction->shape(),
+                                         /*index=*/{1});
+        float transfer_bytes = static_cast<float>(
+            cost_analysis_->base_costs().GetShapeSize(transfer_shape));
+        if (direction == MemoryTransferDirection::kDefaultToAlternate) {
+          outstanding_read_default_queue_.push_back(
+              OutstandingAsyncCopy{instruction, transfer_bytes});
+        } else if (direction == MemoryTransferDirection::kAlternateToDefault) {
+          outstanding_write_default_queue_.push_back(
+              OutstandingAsyncCopy{instruction, transfer_bytes});
+        } else {
+          // The memory access is not a default<->alternate memory copy.
+          continue;
+        }
+      } else if (instruction->opcode() == HloOpcode::kCopyDone ||
+                 (instruction->opcode() == HloOpcode::kAsyncDone &&
+                  instruction->async_wrapped_instruction()->opcode() ==
+                      HloOpcode::kSlice)) {
+        inst_elapsed = SimulateAsyncCopyDone(instruction);
+      } else {
+        // This branch is for the compute instructions.
+        absl::Span<const ShapeIndex> outputs_in_alternate_memory;
+        auto output_it = outputs_in_alternate_memory_map_.find(instruction);
+        if (output_it != outputs_in_alternate_memory_map_.end()) {
+          outputs_in_alternate_memory = absl::MakeSpan(output_it->second);
+        }
+
+        absl::Span<const std::pair<int64_t, ShapeIndex>>
+            operands_in_alternate_memory;
+        auto operand_it = operands_in_alternate_memory_map_.find(instruction);
+        if (operand_it != operands_in_alternate_memory_map_.end())
+          operands_in_alternate_memory = absl::MakeSpan(operand_it->second);
+
+        inst_elapsed = SimulateComputeInstruction(instruction,
+                                                  operands_in_alternate_memory,
+                                                  outputs_in_alternate_memory);
+      }
+      if (inst_elapsed > 0) {
+        float total_trip_count = cost_analysis_->CalculateNestTripCount(
+            instruction, &cost_analysis_cache_);
+        total_elapsed += inst_elapsed * total_trip_count;
+      }
+    }
   }
   return total_elapsed;
 }
--- third_party/xla/xla/service/memory_space_assignment/simulator.h
+++ third_party/xla/xla/service/memory_space_assignment/simulator.h
@@ -16,29 +16,186 @@ limitations under the License.
 #ifndef XLA_SERVICE_MEMORY_SPACE_ASSIGNMENT_SIMULATOR_H_
 #define XLA_SERVICE_MEMORY_SPACE_ASSIGNMENT_SIMULATOR_H_
 
+#include <cstdint>
+#include <list>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/types/span.h"
+#include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/utils/hlo_live_range.h"
 #include "xla/service/memory_space_assignment/allocation.h"
 #include "xla/service/memory_space_assignment/cost_analysis.h"
+#include "xla/shape_util.h"
 
 namespace xla {
 namespace memory_space_assignment {
 
+enum class MemoryTransferDirection {
+  kUnsupported,
+  kDefaultToAlternate,
+  kAlternateToDefault,
+};
+
+// REQUIRES:
+// * async_copy must be an async copy-start or slice-start instruction.
+MemoryTransferDirection GetAsyncCopyDirection(const HloInstruction* async_copy,
+                                              int64_t alternate_memory_space);
+
+// This struct is used to track the outstanding async copy instructions and
+// the remaining bytes required to be accessed.
+struct OutstandingAsyncCopy {
+  const HloInstruction* copy_start_inst;
+  float remaining_bytes_to_transfer;
+  bool operator==(const OutstandingAsyncCopy& other) const {
+    return copy_start_inst == other.copy_start_inst &&
+           remaining_bytes_to_transfer == other.remaining_bytes_to_transfer;
+  }
+};
+
 // A wrapper class around runtime simulator.
 class RuntimeSimulator {
  public:
-  explicit RuntimeSimulator(CostAnalysis* cost_analysis)
-      : cost_analysis_(cost_analysis) {}
-  virtual ~RuntimeSimulator() = default;
-  // This function is used to predict the effectiveness of the memory space
-  // assignment solution. Specifically, it returns the estimated execution time
-  // (in seconds) of the HLO module for the given memory space assignment (i.e.,
-  // ```allocations```).
-  float ComputeEstimatedElapsedTime(const HloLiveRange& hlo_live_range,
-                                    const AllocationSequence& allocations);
+  explicit RuntimeSimulator(CostAnalysis* cost_analysis,
+                            int64_t alternate_memory_space)
+      : cost_analysis_(cost_analysis),
+        alternate_memory_space_(alternate_memory_space) {}
+
+  // This constructor is used to inject the outstanding async copy queues for
+  // testing purpose.
+  explicit RuntimeSimulator(
+      CostAnalysis* cost_analysis, int64_t alternate_memory_space,
+      const std::list<OutstandingAsyncCopy>& outstanding_read_default_queue,
+      const std::list<OutstandingAsyncCopy>& outstanding_write_default_queue)
+      : cost_analysis_(cost_analysis),
+        alternate_memory_space_(alternate_memory_space),
+        outstanding_read_default_queue_(outstanding_read_default_queue),
+        outstanding_write_default_queue_(outstanding_write_default_queue) {}
+
+  ~RuntimeSimulator() = default;
+
+  // This function provides a basic estimate without considering the overhead of
+  // async copies.
+  float SimulateElapsedTimeWithoutAsyncCopies(
+      const HloLiveRange& hlo_live_range,
+      const AllocationSequence& allocations);
+
+  // This function, compared with SimulateElapsedTimeWithoutAsyncCopies
+  // function, provides a more accurate estimated execution time, as it
+  // simulates the default memory communication to estimate the overhead of
+  // async copies.
+  // To simulate the overhead of async copies, we need to maintain two queues to
+  // track the memory access requests that read/write the default memory.
+  // Specifically, for any async copy instruction (copy-start), we push it to
+  // the corresponding queue. When the copy-done instruction is executed, we pop
+  // it (and all the previous copy-start instructions) from the queue and
+  // calculate the execution time of the async copy. Instead of the copy-done
+  // instruction, we also try to drain the queue during computation instruction,
+  // in which default memory is not accessed. The most important feature for the
+  // memory model is sharing the bandwidth. Specifically, if both queues are not
+  // empty, we can only use half of the bandwidth to transfer the request for
+  // each of them in parallel.
+
+  // Here is the overview of the algorithm:
+  // func SimulateElapsedTime():
+  //   read_queue = []
+  //   write_queue = []
+  //   total_elapsed = 0
+  //   for each instruction:
+  //     if instruction is copy-start:
+  //       if instruction is default to alternate memory:
+  //         read_queue.push(instruction)
+  //       else if instruction is alternate to default memory:
+  //         write_queue.push(instruction)
+  //     else if instruction is copy-done:
+  //       # pop instruction from the read/write queue and calculate the
+  //       # execution time of the async copy
+  //       total_elapsed += SimulateAsyncCopyDone()
+  //     else if instruction is compute:
+  //       # Calculate the execution time of the compute instruction.
+  //       total_elapsed += GetInstructionElapsedInAlternateMemory()
+  //       # Get time window in which defaul memory is not accessed.
+  //       idle_time_window = GetDefaultMemoryBandwidthIdleTime()
+  //       # Process read/write queues during the time window.
+  //       ProcessAsyncCopyInTimeWindow(idle_time_window)
+  //     end if
+  //   end for
+  //   return total_elapsed
+  float SimulateElapsedTime(const HloModule* hlo_module,
+                            const HloLiveRange& hlo_live_range,
+                            const AllocationSequence& allocations);
+
+  // This is an auxiliary function for simulating the execution
+  // time for executing a copy-done instruction. It returns the
+  // elapsed time (in seconds) for executing the copy-done instruction.
+  //
+  // This function also updates the passed in queues as we complete async copies
+  // during the simulation.
+  //
+  // We simulate the shared bandwidth for default-alternate memory access.
+  // For example, if the copy-done instruction is a default-write memory
+  // process, and there are outstanding default-read memory processes in the
+  // outstanding_read_default_queue, then we use half of the bandwidth to
+  // process both requests in parallel. Otherwise, we use the full bandwidth to
+  // process the default-write request.
+  float SimulateAsyncCopyDone(const HloInstruction* copy_done_instruction);
+
+  const std::list<OutstandingAsyncCopy>& GetOutstandingReadDefaultQueue() const;
+
+  const std::list<OutstandingAsyncCopy>& GetOutstandingWriteDefaultQueue()
+      const;
+
+  // This is an auxiliary function for simulating the execution
+  // time for a compute instruction. It returns the elapsed time (in seconds)
+  // for executing the compute instruction.
+  //
+  // This function first calculates the elapsed time for the compute instruction
+  // itself. The elapsed time is independent with any outstanding memory
+  // requests.
+  //
+  // Except returning the elapsed time, this function also updates the
+  // outstanding memory requests queue: It calculates the time
+  // window in which the compute instruction does not access the default memory,
+  // and tries to drain the outstanding memory requests in this time window.
+  float SimulateComputeInstruction(
+      const HloInstruction* compute_instruction,
+      absl::Span<const std::pair<int64_t, ShapeIndex>>
+          operands_in_alternate_memory,
+      absl::Span<const ShapeIndex> outputs_in_alternate_memory);
+
+  // This is an auxiliary function which simulates the process of draining
+  // the memory access queue in a given time window. There are two queues
+  // which will share the bandwidth: ```read_queue``` and ```write_queue```
+  // which track the memory access requests that read/write the default
+  // memory. When both of the queues are not empty, the front requests from
+  // both queues equally share the bandwidth. When one of the queue is
+  // empty, the other queue can use the full bandwidth.
+  void ProcessAsyncCopiesInTimeWindow(float time_window);
 
  private:
+  // This function parses the memory space assignment solution and initializes
+  // the maps that record, for each instruction, which outputs and operands are
+  // stored in alternate memory. These maps are used to estimate the runtime of
+  // the HLO module.
+  void InitializeAlternateMemoryMap(const AllocationSequence& allocations);
   const CostAnalysis* cost_analysis_;
   CostAnalysis::Cache cost_analysis_cache_;
+  // Members used for memory model simulation
+  int64_t alternate_memory_space_;
+  std::list<OutstandingAsyncCopy> outstanding_read_default_queue_;
+  std::list<OutstandingAsyncCopy> outstanding_write_default_queue_;
+  absl::flat_hash_map<const HloInstruction*, std::vector<ShapeIndex>>
+      outputs_in_alternate_memory_map_;
+  absl::flat_hash_map<const HloInstruction*,
+                      std::vector<std::pair<int64_t, ShapeIndex>>>
+      operands_in_alternate_memory_map_;
+  // This function updates the queue by updating the front request with the
+  // processed bytes. If the request is completed (no remaining bytes to
+  // process), the function returns the instruction and pop it from the queue.
+  // Otherwise, it returns nullptr.
+  const HloInstruction* RemoveBytesFromQueueIfNotEmpty(
+      std::list<OutstandingAsyncCopy>& async_copy_queue, float processed_bytes);
 };
 }  // namespace memory_space_assignment
 }  // namespace xla
