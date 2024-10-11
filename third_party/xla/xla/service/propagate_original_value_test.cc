intel-ai-tce/xla

[xla] Saves original value information in fusion instructions
Currently TPU logging only prints out the information of a fusion instruction, but not the intermediate values in fused comptation. compare_hlo_output uses origin attribute as a key for comparing instructions, and as a result it cannot compare original values of fusion instructions properly, since original values are saved in the fused root, but not the instruction itself. This is a workaround.

PiperOrigin-RevId: 677923103

xla/service/propagate_original_value_test.cc
@@ -62,7 +62,7 @@ CHECK:   ROOT %[[ADD:.*]] = u32[2]{0:T(256)} add(%[[PAD]], %[[PAD1]]), origin={{
 
 CHECK: ENTRY %test
 CHECK:   %Arg_0 = s32[]{:T(256)} parameter(0), origin={{[{]}}{"Arg_0"}
-CHECK:   ROOT %fusion = u32[2]{0:T(256)} fusion(%Arg_0), kind=kLoop, calls=%fused_computation
+CHECK:   ROOT %fusion = u32[2]{0:T(256)} fusion(%Arg_0), kind=kLoop, calls=%fused_computation, origin={{[{]}}{"concatenate"}
 )");
 }
 

hexagon-geo-surv/tensorflow

third_party/xla/xla/service/propagate_original_value_test.cc
@@ -62,7 +62,7 @@ CHECK:   ROOT %[[ADD:.*]] = u32[2]{0:T(256)} add(%[[PAD]], %[[PAD1]]), origin={{
 
 CHECK: ENTRY %test
 CHECK:   %Arg_0 = s32[]{:T(256)} parameter(0), origin={{[{]}}{"Arg_0"}
-CHECK:   ROOT %pad_add_fusion = u32[2]{0:T(256)} fusion(%Arg_0), kind=kLoop, calls=%fused_computation
+CHECK:   ROOT %pad_add_fusion = u32[2]{0:T(256)} fusion(%Arg_0), kind=kLoop, calls=%fused_computation, origin={{[{]}}{"concatenate"}
 )");
 }
 