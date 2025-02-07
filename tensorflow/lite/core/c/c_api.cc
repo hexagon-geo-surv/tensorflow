hexagon-geo-surv/tensorflow

commit 31a57e22308c6b5a0f2971fb48137613a95366ce

Move signature runner related API function definitions from c_api_experimental.cc to c_api.cc, since those API functions are not experimental any more.
Also, some include-what-you-use fixes.

PiperOrigin-RevId: 723014570

tensorflow/lite/core/c/BUILD
@@ -100,15 +100,21 @@ tflite_cc_library_with_c_headers_test(
         ":c_api_types",
         ":operator",
         "//tensorflow/lite:builtin_ops",
+        "//tensorflow/lite:mutable_op_resolver",
+        "//tensorflow/lite:signature_runner",
+        "//tensorflow/lite:stderr_reporter",
         "//tensorflow/lite:version",
         "//tensorflow/lite/c:c_api_internal",
         "//tensorflow/lite/c:common_internal",
         "//tensorflow/lite/core:create_op_resolver_header",
-        "//tensorflow/lite/core:framework",
+        "//tensorflow/lite/core:framework_stable",
+        "//tensorflow/lite/core/api",
         "//tensorflow/lite/core/async/c:types",
         "//tensorflow/lite/delegates:interpreter_utils",
         "//tensorflow/lite/delegates/nnapi:nnapi_delegate",
         "//tensorflow/lite/kernels/internal:compatibility",
+        "//tensorflow/lite/profiling/telemetry:profiler",
+        "//tensorflow/lite/schema:schema_fbs",
     ],
     alwayslink = 1,  # Why?? TODO(b/161243354): eliminate this.
 )
@@ -144,17 +150,23 @@ tflite_cc_library_with_c_headers_test(
         ":c_api_types",
         ":operator_without_alwayslink",
         "//tensorflow/lite:builtin_ops",
-        "//tensorflow/lite:framework",
+        "//tensorflow/lite:framework_stable",
+        "//tensorflow/lite:mutable_op_resolver",
+        "//tensorflow/lite:signature_runner",
+        "//tensorflow/lite:stderr_reporter",
         "//tensorflow/lite:version",
         "//tensorflow/lite/c:c_api_internal",
         "//tensorflow/lite/c:common_internal",
         "//tensorflow/lite/core:create_op_resolver_header",
-        "//tensorflow/lite/core:framework",
+        "//tensorflow/lite/core:framework_stable",
+        "//tensorflow/lite/core/api",
         "//tensorflow/lite/core/async/c:types",
         "//tensorflow/lite/delegates:interpreter_utils",
         "//tensorflow/lite/delegates/nnapi:nnapi_delegate",
         "//tensorflow/lite/kernels:kernel_util",
         "//tensorflow/lite/kernels/internal:compatibility",
+        "//tensorflow/lite/profiling/telemetry:profiler",
+        "//tensorflow/lite/schema:schema_fbs",
     ],
 )
 

tensorflow/lite/core/c/c_api.cc
@@ -14,6 +14,10 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/core/c/c_api.h"
 
+#include <stddef.h>
+#include <string.h>
+
+#include <cstdint>
 #include <memory>
 #include <mutex>  // NOLINT
 #include <utility>
@@ -22,12 +26,21 @@ limitations under the License.
 #include "tensorflow/lite/builtin_ops.h"
 #include "tensorflow/lite/c/c_api_internal.h"
 #include "tensorflow/lite/c/common_internal.h"
+#include "tensorflow/lite/core/api/error_reporter.h"
+#include "tensorflow/lite/core/api/op_resolver.h"
+#include "tensorflow/lite/core/c/c_api_types.h"
 #include "tensorflow/lite/core/create_op_resolver.h"
 #include "tensorflow/lite/core/interpreter.h"
-#include "tensorflow/lite/core/model.h"
+#include "tensorflow/lite/core/interpreter_builder.h"
+#include "tensorflow/lite/core/model_builder.h"
 #include "tensorflow/lite/delegates/interpreter_utils.h"
 #include "tensorflow/lite/delegates/nnapi/nnapi_delegate.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
+#include "tensorflow/lite/mutable_op_resolver.h"
+#include "tensorflow/lite/profiling/telemetry/profiler.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/signature_runner.h"
+#include "tensorflow/lite/stderr_reporter.h"
 #include "tensorflow/lite/version.h"
 
 namespace {
@@ -274,6 +287,92 @@ TfLiteStatus TfLiteTensorCopyToBuffer(const TfLiteTensor* tensor,
 }
 
 // LINT.ThenChange(//tensorflow/lite/experimental/examples/unity/TensorFlowLitePlugin/Assets/TensorFlowLite/SDK/Scripts/Interpreter.cs)
+
+int32_t TfLiteInterpreterGetSignatureCount(
+    const TfLiteInterpreter* interpreter) {
+  return static_cast<int32_t>(interpreter->impl->signature_keys().size());
+}
+
+const char* TfLiteInterpreterGetSignatureKey(
+    const TfLiteInterpreter* interpreter, int32_t signature_index) {
+  int32_t signature_count = TfLiteInterpreterGetSignatureCount(interpreter);
+  if (signature_index < 0 || signature_index >= signature_count) {
+    return nullptr;
+  }
+  return interpreter->impl->signature_keys()[signature_index]->c_str();
+}
+
+TfLiteSignatureRunner* TfLiteInterpreterGetSignatureRunner(
+    const TfLiteInterpreter* interpreter, const char* signature_key) {
+  tflite::SignatureRunner* signature_runner =
+      interpreter->impl->GetSignatureRunner(signature_key);
+  if (!signature_runner) return nullptr;
+  return new TfLiteSignatureRunner{signature_runner};
+}
+
+size_t TfLiteSignatureRunnerGetInputCount(
+    const TfLiteSignatureRunner* signature_runner) {
+  return signature_runner->impl->input_size();
+}
+
+const char* TfLiteSignatureRunnerGetInputName(
+    const TfLiteSignatureRunner* signature_runner, const int32_t input_index) {
+  int32_t input_count = TfLiteSignatureRunnerGetInputCount(signature_runner);
+  if (input_index < 0 || input_index >= input_count) {
+    return nullptr;
+  }
+  return signature_runner->impl->input_names()[input_index];
+}
+
+TfLiteStatus TfLiteSignatureRunnerResizeInputTensor(
+    TfLiteSignatureRunner* signature_runner, const char* input_name,
+    const int* input_dims, int32_t input_dims_size) {
+  std::vector<int> dims{input_dims, input_dims + input_dims_size};
+  return signature_runner->impl->ResizeInputTensorStrict(input_name, dims);
+}
+
+TfLiteStatus TfLiteSignatureRunnerAllocateTensors(
+    TfLiteSignatureRunner* signature_runner) {
+  return signature_runner->impl->AllocateTensors();
+}
+
+TfLiteTensor* TfLiteSignatureRunnerGetInputTensor(
+    TfLiteSignatureRunner* signature_runner, const char* input_name) {
+  return signature_runner->impl->input_tensor(input_name);
+}
+
+TfLiteStatus TfLiteSignatureRunnerInvoke(
+    TfLiteSignatureRunner* signature_runner) {
+  return signature_runner->impl->Invoke();
+}
+
+size_t TfLiteSignatureRunnerGetOutputCount(
+    const TfLiteSignatureRunner* signature_runner) {
+  return signature_runner->impl->output_size();
+}
+
+const char* TfLiteSignatureRunnerGetOutputName(
+    const TfLiteSignatureRunner* signature_runner, int32_t output_index) {
+  int32_t output_count = TfLiteSignatureRunnerGetOutputCount(signature_runner);
+  if (output_index < 0 || output_index >= output_count) {
+    return nullptr;
+  }
+  return signature_runner->impl->output_names()[output_index];
+}
+
+const TfLiteTensor* TfLiteSignatureRunnerGetOutputTensor(
+    const TfLiteSignatureRunner* signature_runner, const char* output_name) {
+  return signature_runner->impl->output_tensor(output_name);
+}
+
+TfLiteStatus TfLiteSignatureRunnerCancel(
+    TfLiteSignatureRunner* signature_runner) {
+  return signature_runner->impl->Cancel();
+}
+
+void TfLiteSignatureRunnerDelete(TfLiteSignatureRunner* signature_runner) {
+  delete signature_runner;
+}
 
 }  // extern "C"
 

tensorflow/lite/c/BUILD
@@ -299,7 +299,7 @@ cc_test(
     deps = [
         ":c_api",
         "//tensorflow/lite/core/c:c_api",
-        "//tensorflow/lite/core/c:c_api_experimental",
+        "//tensorflow/lite/core/c:common",
         "@com_google_googletest//:gtest_main",
     ],
 )

tensorflow/lite/c/c_api_signature_runner_test.cc
@@ -19,7 +19,7 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "tensorflow/lite/core/c/c_api.h"
-#include "tensorflow/lite/core/c/c_api_experimental.h"
+#include "tensorflow/lite/core/c/common.h"
 
 namespace tflite {
 namespace {

tensorflow/lite/core/c/c_api_experimental.cc
@@ -193,11 +193,6 @@ int32_t TfLiteInterpreterGetOutputTensorIndex(
   return interpreter->impl->outputs()[output_index];
 }
 
-int32_t TfLiteInterpreterGetSignatureCount(
-    const TfLiteInterpreter* interpreter) {
-  return static_cast<int32_t>(interpreter->impl->signature_keys().size());
-}
-
 TfLiteStatus TfLiteInterpreterSetBufferHandle(TfLiteInterpreter* interpreter,
                                               TfLiteTensor* tensor,
                                               TfLiteBufferHandle buffer_handle,
@@ -231,87 +226,6 @@ TfLiteStatus TfLiteInterpreterSetCustomAllocationForTensor(
 TfLiteStatus TfLiteInterpreterEnsureTensorDataIsReadable(
     TfLiteInterpreter* interpreter, int tensor_index) {
   return interpreter->impl->EnsureTensorDataIsReadable(tensor_index);
-}
-
-const char* TfLiteInterpreterGetSignatureKey(
-    const TfLiteInterpreter* interpreter, int32_t signature_index) {
-  int32_t signature_count = TfLiteInterpreterGetSignatureCount(interpreter);
-  if (signature_index < 0 || signature_index >= signature_count) {
-    return nullptr;
-  }
-  return interpreter->impl->signature_keys()[signature_index]->c_str();
-}
-
-TfLiteSignatureRunner* TfLiteInterpreterGetSignatureRunner(
-    const TfLiteInterpreter* interpreter, const char* signature_key) {
-  tflite::SignatureRunner* signature_runner =
-      interpreter->impl->GetSignatureRunner(signature_key);
-  if (!signature_runner) return nullptr;
-  return new TfLiteSignatureRunner{signature_runner};
-}
-
-size_t TfLiteSignatureRunnerGetInputCount(
-    const TfLiteSignatureRunner* signature_runner) {
-  return signature_runner->impl->input_size();
-}
-
-const char* TfLiteSignatureRunnerGetInputName(
-    const TfLiteSignatureRunner* signature_runner, const int32_t input_index) {
-  int32_t input_count = TfLiteSignatureRunnerGetInputCount(signature_runner);
-  if (input_index < 0 || input_index >= input_count) {
-    return nullptr;
-  }
-  return signature_runner->impl->input_names()[input_index];
-}
-
-TfLiteStatus TfLiteSignatureRunnerResizeInputTensor(
-    TfLiteSignatureRunner* signature_runner, const char* input_name,
-    const int* input_dims, int32_t input_dims_size) {
-  std::vector<int> dims{input_dims, input_dims + input_dims_size};
-  return signature_runner->impl->ResizeInputTensorStrict(input_name, dims);
-}
-
-TfLiteStatus TfLiteSignatureRunnerAllocateTensors(
-    TfLiteSignatureRunner* signature_runner) {
-  return signature_runner->impl->AllocateTensors();
-}
-
-TfLiteTensor* TfLiteSignatureRunnerGetInputTensor(
-    TfLiteSignatureRunner* signature_runner, const char* input_name) {
-  return signature_runner->impl->input_tensor(input_name);
-}
-
-TfLiteStatus TfLiteSignatureRunnerInvoke(
-    TfLiteSignatureRunner* signature_runner) {
-  return signature_runner->impl->Invoke();
-}
-
-size_t TfLiteSignatureRunnerGetOutputCount(
-    const TfLiteSignatureRunner* signature_runner) {
-  return signature_runner->impl->output_size();
-}
-
-const char* TfLiteSignatureRunnerGetOutputName(
-    const TfLiteSignatureRunner* signature_runner, int32_t output_index) {
-  int32_t output_count = TfLiteSignatureRunnerGetOutputCount(signature_runner);
-  if (output_index < 0 || output_index >= output_count) {
-    return nullptr;
-  }
-  return signature_runner->impl->output_names()[output_index];
-}
-
-const TfLiteTensor* TfLiteSignatureRunnerGetOutputTensor(
-    const TfLiteSignatureRunner* signature_runner, const char* output_name) {
-  return signature_runner->impl->output_tensor(output_name);
-}
-
-TfLiteStatus TfLiteSignatureRunnerCancel(
-    TfLiteSignatureRunner* signature_runner) {
-  return signature_runner->impl->Cancel();
-}
-
-void TfLiteSignatureRunnerDelete(TfLiteSignatureRunner* signature_runner) {
-  delete signature_runner;
 }
 
 void TfLiteInterpreterOptionsSetTelemetryProfiler(

commit c94192f8585baec4956108a9030976f2e4de03f6

Fix build breakage introduced in https://github.com/tensorflow/tensorflow/commit/31a57e22308c6b5a0f2971fb48137613a95366ce
From within `core/` directory, depend on the `signature_runner` target
that is within `core/`, rather than the  `signature_runner`
target that is outside `core`.  That is, depend on
`tensorflow/lite/core:signature_runner` rather than
`tensorflow/lite:signature_runner`.

PiperOrigin-RevId: 724293796

tensorflow/lite/core/c/BUILD
@@ -101,13 +101,13 @@ tflite_cc_library_with_c_headers_test(
         ":operator",
         "//tensorflow/lite:builtin_ops",
         "//tensorflow/lite:mutable_op_resolver",
-        "//tensorflow/lite:signature_runner",
         "//tensorflow/lite:stderr_reporter",
         "//tensorflow/lite:version",
         "//tensorflow/lite/c:c_api_internal",
         "//tensorflow/lite/c:common_internal",
         "//tensorflow/lite/core:create_op_resolver_header",
         "//tensorflow/lite/core:framework_stable",
+        "//tensorflow/lite/core:signature_runner",
         "//tensorflow/lite/core/api",
         "//tensorflow/lite/core/async/c:types",
         "//tensorflow/lite/delegates:interpreter_utils",
@@ -150,15 +150,14 @@ tflite_cc_library_with_c_headers_test(
         ":c_api_types",
         ":operator_without_alwayslink",
         "//tensorflow/lite:builtin_ops",
-        "//tensorflow/lite:framework_stable",
         "//tensorflow/lite:mutable_op_resolver",
-        "//tensorflow/lite:signature_runner",
         "//tensorflow/lite:stderr_reporter",
         "//tensorflow/lite:version",
         "//tensorflow/lite/c:c_api_internal",
         "//tensorflow/lite/c:common_internal",
         "//tensorflow/lite/core:create_op_resolver_header",
         "//tensorflow/lite/core:framework_stable",
+        "//tensorflow/lite/core:signature_runner",
         "//tensorflow/lite/core/api",
         "//tensorflow/lite/core/async/c:types",
         "//tensorflow/lite/delegates:interpreter_utils",

tensorflow/lite/core/c/c_api.cc
@@ -33,13 +33,13 @@ limitations under the License.
 #include "tensorflow/lite/core/interpreter.h"
 #include "tensorflow/lite/core/interpreter_builder.h"
 #include "tensorflow/lite/core/model_builder.h"
+#include "tensorflow/lite/core/signature_runner.h"
 #include "tensorflow/lite/delegates/interpreter_utils.h"
 #include "tensorflow/lite/delegates/nnapi/nnapi_delegate.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
 #include "tensorflow/lite/mutable_op_resolver.h"
 #include "tensorflow/lite/profiling/telemetry/profiler.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/signature_runner.h"
 #include "tensorflow/lite/stderr_reporter.h"
 #include "tensorflow/lite/version.h"
 