MohnishKalia/xla

[XLA] Check array operator() access is within bounds.
PiperOrigin-RevId: 738978075

xla/array.h
@@ -365,6 +365,9 @@ class Array {
                           const T&>::type
   operator()(Dims... dims) const {
     CHECK_EQ(sizeof...(dims), num_dimensions());
+    // Check each index is within the bounds of the array.
+    int64_t i = 0;
+    ([&] { CHECK_LT(dims, sizes_[i++]); }(), ...);
     // We are using a std::array to avoid having to allocate memory in this
     // function for performance reasons.
     std::array<int64_t, sizeof...(dims)> indexes{

hexagon-geo-surv/tensorflow

third_party/xla/xla/array.h
@@ -365,6 +365,9 @@ class Array {
                           const T&>::type
   operator()(Dims... dims) const {
     CHECK_EQ(sizeof...(dims), num_dimensions());
+    // Check each index is within the bounds of the array.
+    int64_t i = 0;
+    ([&] { DCHECK_LT(dims, sizes_[i++]); }(), ...);
     // We are using a std::array to avoid having to allocate memory in this
     // function for performance reasons.
     std::array<int64_t, sizeof...(dims)> indexes{