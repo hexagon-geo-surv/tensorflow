CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

[XLA:GPU] update ncu-rep parser to work with multiple kernels
Now the correctly processes profiles with multiple kernels as a list of rows (previously replaced kernels with the same name making analysis of multiple kernels impossible).
To enable processing of complex profiles we now have aggregation and filter. Aggregation looks at the prefix of the metric and might not be completely correct at the moment.

Filtering allows to pick a subset of kernels and only aggregate ones that we care about, e.g. --filter='after:name:setup' will only consider kernels that were run after the kernel with "setup" in the name. Again filter is somewhat simplistic but overall that provides an ability to e.g. sum durations or memory transfer across multiple kernels that nsight compute does not provide.

Misc:

- Updated argument names, profile name is passed as an argument, making the tool easier to use.

- To properly aggregate values it now always works with basic units, for example 'ns' instead of 's' or 'ms'.

PiperOrigin-RevId: 744698701

third_party/xla/xla/backends/gpu/codegen/tools/ncu_rep_lib.py
@@ -19,13 +19,18 @@ Separate library from ncu_rep_main.py to enable unit tests.
 
 import csv
 import json
+import logging
+import re
 from typing import TextIO
 from absl import app
+
+KERNEL_NAME_FIELD = "Kernel Name"
+KERNEL_ID_FIELD = "ID"
 
 
 def get_metrics_by_kernel(
     rows: list[list[str]],
-) -> dict[str, dict[str, tuple[str, str]]]:
+) -> list[dict[str, tuple[str, str]]]:
   """Converts ncu-rep table to a dictionary of metrics by kernel.
 
   Args:
@@ -38,43 +43,96 @@ def get_metrics_by_kernel(
   units = rows[1]
   for i, name in enumerate(rows[0]):
     name_index[name] = i
-  results = {}
+  results = []
   for kernel in rows[2:]:
     values = {}
     for idx, name in enumerate(rows[0]):
       values[name] = (kernel[idx], units[idx])
-    kernel_name = values["Kernel Name"][0]
-    results[kernel_name] = values
+    results.append(values)
   return results
 
 
-def get_kernel_metrics_rows(
-    metrics: list[str],
-    all_metrics: dict[str, dict[str, tuple[str, str]]],
-    kernel_name: str,
+def aggregate(a: list[float], metric_name: str):
+  if str.endswith(metric_name, ".max"):
+    return max(a)
+  if str.endswith(metric_name, ".min"):
+    return min(a)
+  if str.endswith(metric_name, ".sum"):
+    return sum(a)
+  return a[0]
+
+
+def aggregate_kernel_metrics(
+    metrics: list[str], rows: list[dict[str, tuple[str, str]]]
 ) -> list[list[str]]:
   """Returns the metrics to print for the given kernel.
 
   Args:
     metrics: list of metrics names to print
-    all_metrics: dictionary of metrics by kernel, extracted from ncu-rep table
-    kernel_name: kernel name to print, returns first kernel if empty
+    rows: dictionary of metrics by kernel, extracted from ncu-rep table
 
   Returns:
     list of rows [name, value, unit] per metric.
   """
-  if not all_metrics:
+  if not rows:
     raise app.UsageError("no metrics found")
-  for kernel, vals in all_metrics.items():
-    if kernel_name and kernel != kernel_name:
-      continue
-    result = []
+  results = {}  # name -> (float[], unit)
+  for vals in rows:
     for name in metrics:
       if name not in vals:
         raise app.UsageError(f"metric '{name}' is not found")
-      result.append([name, vals[name][0], vals[name][1]])
-    return result
-  raise app.UsageError(f"kernel '{kernel_name}' is not found")
+      v, u = vals[name]
+      if name not in results:
+        results[name] = ([], u)
+      if results[name][1] != u:
+        # That should not happen with `--print-units base` but left as a
+        # safety check.
+        raise app.UsageError(f"unit mismatch for metric '{name}'")
+      results[name][0].append(float(v.replace(",", "")))
+  rows = []
+  for name, (values, unit) in results.items():
+    a = aggregate(values, name)
+    rows.append([name, f"{a:.2f}", unit])
+  return rows
+
+
+def filter_kernels(
+    kernels: list[dict[str, tuple[str, str]]], condition: str
+) -> list[dict[str, tuple[str, str]]]:
+  """Filters kernels by a condition.
+
+  Args:
+    kernels: list of kernel tuples, extracted from ncu-rep CSV
+    condition: filter condition. Supported filter expressions: 'id:<value>' -
+      kernel with an ID 'name:<regex>' - kernel with a name matching the regex
+      'after:<filter>' - kernels after the last kernel matching the filter.
+
+  Returns:
+    matching kernels
+  """
+  if condition.startswith("id:"):
+    i = condition.removeprefix("id:")
+    return [v for v in kernels if v[KERNEL_ID_FIELD][0] == i]
+  if condition.startswith("name:"):
+    r = condition.removeprefix("name:")
+    return [v for v in kernels if re.match(r, v[KERNEL_NAME_FIELD][0])]
+  if condition.startswith("after:"):
+    r = condition.removeprefix("after:")
+    sub = filter_kernels(kernels, r)
+    if not sub:
+      logging.warning("no kernels matched '%s', 'after:' has no effect", r)
+      return kernels
+    after_id = sub[-1][KERNEL_ID_FIELD][0]
+    add = False
+    res = []
+    for v in kernels:
+      if v[KERNEL_ID_FIELD][0] == after_id:
+        add = True
+        continue
+      if add:
+        res.append(v)
+    return res
+  raise app.UsageError(f"unsupported filter: {condition}")
 
 
 def write_metrics_markdown(out: TextIO, metrics: list[list[str]]):

hexagon-geo-surv/tensorflow

third_party/xla/xla/backends/gpu/codegen/tools/ncu_rep_lib.py
@@ -19,13 +19,18 @@ Separate library from ncu_rep_main.py to enable unit tests.
 
 import csv
 import json
+import logging
+import re
 from typing import TextIO
 from absl import app
+
+KERNEL_NAME_FIELD = "Kernel Name"
+KERNEL_ID_FIELD = "ID"
 
 
 def get_metrics_by_kernel(
     rows: list[list[str]],
-) -> dict[str, dict[str, tuple[str, str]]]:
+) -> list[dict[str, tuple[str, str]]]:
   """Converts ncu-rep table to a dictionary of metrics by kernel.
 
   Args:
@@ -38,43 +43,109 @@ def get_metrics_by_kernel(
   units = rows[1]
   for i, name in enumerate(rows[0]):
     name_index[name] = i
-  results = {}
+  results = []
   for kernel in rows[2:]:
     values = {}
     for idx, name in enumerate(rows[0]):
       values[name] = (kernel[idx], units[idx])
-    kernel_name = values["Kernel Name"][0]
-    results[kernel_name] = values
+    results.append(values)
   return results
 
 
-def get_kernel_metrics_rows(
-    metrics: list[str],
-    all_metrics: dict[str, dict[str, tuple[str, str]]],
-    kernel_name: str,
+def aggregate(metric_values: list[float], metric_name: str):
+  """Aggregates metric values using a function based on the metric name.
+
+  If metric name does not match any of the known patterns, the first value from
+  the input list is returned.
+
+  Args:
+    metric_values: list of metric values, floats
+    metric_name: metric name
+
+  Returns:
+    aggregated metric value
+  """
+  if str.endswith(metric_name, ".max"):
+    return max(metric_values)
+  if str.endswith(metric_name, ".min"):
+    return min(metric_values)
+  if str.endswith(metric_name, ".sum"):
+    return sum(metric_values)
+  return metric_values[0]
+
+
+def aggregate_kernel_metrics(
+    metrics: list[str], kernel_metrics: list[dict[str, tuple[str, str]]]
 ) -> list[list[str]]:
-  """Returns the metrics to print for the given kernel.
+  """Aggregates and returns the metrics for the given kernels.
 
   Args:
     metrics: list of metrics names to print
-    all_metrics: dictionary of metrics by kernel, extracted from ncu-rep table
-    kernel_name: kernel name to print, returns first kernel if empty
+    kernel_metrics: dictionary of metrics by kernel
 
   Returns:
     list of rows [name, value, unit] per metric.
   """
-  if not all_metrics:
+  if not kernel_metrics:
     raise app.UsageError("no metrics found")
-  for kernel, vals in all_metrics.items():
-    if kernel_name and kernel != kernel_name:
-      continue
-    result = []
+  results: dict[str, tuple[list[float], str]] = {}  # name -> (float[], unit)
+  for vals in kernel_metrics:
     for name in metrics:
       if name not in vals:
         raise app.UsageError(f"metric '{name}' is not found")
-      result.append([name, vals[name][0], vals[name][1]])
-    return result
-  raise app.UsageError(f"kernel '{kernel_name}' is not found")
+      value, unit = vals[name]
+      if name not in results:
+        results[name] = ([], unit)
+      if results[name][1] != unit:
+        # That should not happen with `--print-units base` but left as a
+        # safety check.
+        raise app.UsageError(f"unit mismatch for metric '{name}'")
+      # Remove commans from the integer part of the value, e.g. '1,234.56'.
+      results[name][0].append(float(value.replace(",", "")))
+  kernel_metrics = []
+  for name, (values, unit) in results.items():
+    a = aggregate(values, name)
+    kernel_metrics.append([name, f"{a:.2f}", unit])
+  return kernel_metrics
+
+
+def filter_kernels(
+    kernels: list[dict[str, tuple[str, str]]], condition: str
+) -> list[dict[str, tuple[str, str]]]:
+  """Filters kernels by a condition.
+
+  Args:
+    kernels: list of kernel tuples, extracted from ncu-rep CSV
+    condition: filter condition. Supported filter expressions: 'id:<value>' -
+      kernel with an ID 'name:<regex>' - kernel with a name matching the regex
+      'after:<filter>' - kernels after the last kernel matching the filter.
+
+  Returns:
+    matching kernels
+  """
+  if condition.startswith("id:"):
+    i = condition.removeprefix("id:")
+    return [v for v in kernels if v[KERNEL_ID_FIELD][0] == i]
+  if condition.startswith("name:"):
+    r = condition.removeprefix("name:")
+    return [v for v in kernels if re.search(r, v[KERNEL_NAME_FIELD][0])]
+  if condition.startswith("after:"):
+    r = condition.removeprefix("after:")
+    sub = filter_kernels(kernels, r)
+    if not sub:
+      logging.warning("no kernels matched '%s', 'after:' has no effect", r)
+      return kernels
+    after_id = sub[-1][KERNEL_ID_FIELD][0]
+    add = False
+    res = []
+    for v in kernels:
+      if v[KERNEL_ID_FIELD][0] == after_id:
+        add = True
+        continue
+      if add:
+        res.append(v)
+    return res
+  raise app.UsageError(f"unsupported filter: {condition}")
 
 
 def write_metrics_markdown(out: TextIO, metrics: list[list[str]]):