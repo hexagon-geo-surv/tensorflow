51a0335b74bd60cdee7cfcb1b4de3d9812cce8b0
Vadym Matsishevskyi <vam@google.com>
2025-04-24 01:50:41 -0700

    Major deps udpate: -Protobuf 5.28.3 -Grpc 1.68.2 -Abseil: LTS 20240116.3 -Plus some other transitive/related deps, riegeli and pybind11 in particular. -rules_python & rules_cc will be updated in a subsequent CL as they are their own can of worms, plus there are a few pending changes in rules_python which were not pushed yet.
    This also switches default protobuf implementaiton we rely on for bazel builds from cpp to upb, meaning all projects dependin on htis one must be built with build --action_env=PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=upb.
    
    PiperOrigin-RevId: 750901588

--- tensorflow/core/common_runtime/graph_constructor_fuzz.cc
+++ tensorflow/core/common_runtime/graph_constructor_fuzz.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include <vector>
 
 #include "fuzztest/fuzztest.h"
+#include "absl/strings/string_view.h"
 #include "tensorflow/core/common_runtime/graph_constructor.h"
 #include "tensorflow/core/public/session.h"
 
@@ -114,12 +115,8 @@ class EmptyErrorCollector : public protobuf::io::ErrorCollector {
  public:
   EmptyErrorCollector() {}
   ~EmptyErrorCollector() override {}
-  void AddError(int line, int column, const std::string& message) override {
-    // log error
-  }
-  void AddWarning(int line, int column, const std::string& message) override {
-    // log warning
-  }
+  void RecordError(int line, protobuf::io::ColumnNumber column,
+                   absl::string_view message) override {}
 };
 
 std::vector<std::string> ops = {
