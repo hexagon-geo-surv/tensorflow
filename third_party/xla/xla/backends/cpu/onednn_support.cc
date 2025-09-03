openxla/xla

PR #30328: [XLA:CPU][oneDNN] Update on the criterias for rewriting Dot to oneDNN Matmul
Imported from GitHub PR https://github.com/openxla/xla/pull/30328

Update on Dot to oneDNN Matmul rewriting in terms of data type, shapes, and canonical order.
Copybara import of the project:

--
5aeeeecc5d4fdfc1bfc8b20c17a0b5e3337f68e4 by Yimei Sun <yimei.sun@intel.com>:

[XLA:CPU][oneDNN] Update on the criterias for rewriting Dot to oneDNN Matmul

Merging this change closes #30328

FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/30328 from Intel-tensorflow:yimei/update_onednn_dot_rewritter 5aeeeecc5d4fdfc1bfc8b20c17a0b5e3337f68e4
PiperOrigin-RevId: 799962371

xla/backends/cpu/onednn_support.cc
@@ -15,31 +15,98 @@ limitations under the License.
 
 #include "xla/backends/cpu/onednn_support.h"
 
+#include "absl/log/log.h"
 #include "absl/status/statusor.h"
+#include "dnnl.h"  // NOLINT: for DNNL_MAX_NDIMS
+#include "xla/backends/cpu/codegen/target_machine_features.h"
 #include "xla/backends/cpu/runtime/dot_lib.h"
 #include "xla/shape.h"
 #include "xla/xla_data.pb.h"
+#include "tsl/platform/cpu_info.h"
 
 namespace xla::cpu {
 
+bool IsOneDnnSupportedDType(PrimitiveType dtype) {
+  using tsl::port::CPUFeature;
+  switch (dtype) {
+    case F32:
+      return true;
+    case BF16:
+      return TestCPUFeature(CPUFeature::AVX512F) ||
+             TestCPUFeature(CPUFeature::AVX_NE_CONVERT) ||
+             TestCPUFeature(CPUFeature::AMX_BF16);
+    case F16:
+      return (TestCPUFeature(CPUFeature::AVX512BW) &&
+              (TestCPUFeature(CPUFeature::AVX512_FP16) ||
+               TestCPUFeature(CPUFeature::AMX_FP16))) ||
+             TestCPUFeature(CPUFeature::AVX_NE_CONVERT);
+    default:
+      return false;
+  }
+}
+
+bool IsOneDnnSupportedDType(PrimitiveType dtype,
+                            const TargetMachineFeatures* cpu_features) {
+  if (dtype == F32) {
+    return true;
+  }
+
+  if (cpu_features == nullptr) {
+    return IsOneDnnSupportedDType(dtype);
+  }
+
+  if (dtype == BF16) {
+    return cpu_features->has_avx512bf16();
+  }
+  if (dtype == F16) {
+    return cpu_features->has_avx512fp16();
+  }
+
+  return false;
+}
+
 absl::StatusOr<bool> IsOneDnnDotSupported(
     const DotDimensionNumbers& dot_dimensions, const Shape& lhs_shape,
-    const Shape& rhs_shape, const Shape& out_shape) {
-  // TODO(penporn): Support other element types.
-  if (lhs_shape.element_type() != F32 || rhs_shape.element_type() != F32 ||
-      out_shape.element_type() != F32) {
+    const Shape& rhs_shape, const Shape& out_shape,
+    const TargetMachineFeatures* cpu_features) {
+  if (lhs_shape.element_type() != rhs_shape.element_type() ||
+      lhs_shape.element_type() != out_shape.element_type()) {
+    return false;
+  }
+  if (!IsOneDnnSupportedDType(out_shape.element_type(), cpu_features)) {
     return false;
   }
 
-  TF_ASSIGN_OR_RETURN(DotShape dot_shape, GetDotShape(dot_dimensions, lhs_shape,
-                                                      rhs_shape, out_shape));
+  if (ShapeUtil::IsZeroElementArray(lhs_shape) ||
+      ShapeUtil::IsZeroElementArray(rhs_shape) ||
+      ShapeUtil::IsZeroElementArray(out_shape)) {
+    return false;
+  }
 
-  TF_ASSIGN_OR_RETURN(DotCanonicalDims dot_canonical_dims,
-                      GetDotCanonicalDims(dot_dimensions, dot_shape));
+  // NOLINTNEXTLINE: Use dnnl.hpp for DNNL_MAX_NDIMS for now.
+  if (lhs_shape.dimensions_size() > DNNL_MAX_NDIMS ||
+      rhs_shape.dimensions_size() > DNNL_MAX_NDIMS ||
+      lhs_shape.dimensions_size() != rhs_shape.dimensions_size()) {
+    return false;
+  }
 
-  // Restrict support to no transposes and row-major layouts for now.
-  return dot_canonical_dims.lhs_canonical && dot_canonical_dims.rhs_canonical &&
-         !dot_canonical_dims.lhs_column_major &&
+  auto dot_shape_result =
+      GetDotShape(dot_dimensions, lhs_shape, rhs_shape, out_shape);
+  if (!dot_shape_result.ok()) {
+    VLOG(2) << "GetDotShape Error: " << dot_shape_result.status();
+    return false;
+  }
+  DotShape dot_shape = dot_shape_result.value();
+
+  auto dot_canonical_result = GetDotCanonicalDims(dot_dimensions, dot_shape);
+  if (!dot_canonical_result.ok()) {
+    VLOG(2) << "GetDotCanonicalDims Error: " << dot_canonical_result.status();
+    return false;
+  }
+  DotCanonicalDims dot_canonical_dims = dot_canonical_result.value();
+
+  // Restrict support to row-major layouts.
+  return !dot_canonical_dims.lhs_column_major &&
          !dot_canonical_dims.rhs_column_major;
 }
 

hexagon-geo-surv/tensorflow

third_party/xla/xla/backends/cpu/onednn_support.cc
@@ -15,31 +15,98 @@ limitations under the License.
 
 #include "xla/backends/cpu/onednn_support.h"
 
+#include "absl/log/log.h"
 #include "absl/status/statusor.h"
+#include "dnnl.hpp"  // NOLINT: for DNNL_MAX_NDIMS
+#include "xla/backends/cpu/codegen/target_machine_features.h"
 #include "xla/backends/cpu/runtime/dot_lib.h"
 #include "xla/shape.h"
 #include "xla/xla_data.pb.h"
+#include "tsl/platform/cpu_info.h"
 
 namespace xla::cpu {
 
+bool IsOneDnnSupportedDType(PrimitiveType dtype) {
+  using tsl::port::CPUFeature;
+  switch (dtype) {
+    case F32:
+      return true;
+    case BF16:
+      return TestCPUFeature(CPUFeature::AVX512F) ||
+             TestCPUFeature(CPUFeature::AVX_NE_CONVERT) ||
+             TestCPUFeature(CPUFeature::AMX_BF16);
+    case F16:
+      return (TestCPUFeature(CPUFeature::AVX512BW) &&
+              (TestCPUFeature(CPUFeature::AVX512_FP16) ||
+               TestCPUFeature(CPUFeature::AMX_FP16))) ||
+             TestCPUFeature(CPUFeature::AVX_NE_CONVERT);
+    default:
+      return false;
+  }
+}
+
+bool IsOneDnnSupportedDType(PrimitiveType dtype,
+                            const TargetMachineFeatures* cpu_features) {
+  if (dtype == F32) {
+    return true;
+  }
+
+  if (cpu_features == nullptr) {
+    return IsOneDnnSupportedDType(dtype);
+  }
+
+  if (dtype == BF16) {
+    return cpu_features->has_avx512bf16();
+  }
+  if (dtype == F16) {
+    return cpu_features->has_avx512fp16();
+  }
+
+  return false;
+}
+
 absl::StatusOr<bool> IsOneDnnDotSupported(
     const DotDimensionNumbers& dot_dimensions, const Shape& lhs_shape,
-    const Shape& rhs_shape, const Shape& out_shape) {
-  // TODO(penporn): Support other element types.
-  if (lhs_shape.element_type() != F32 || rhs_shape.element_type() != F32 ||
-      out_shape.element_type() != F32) {
+    const Shape& rhs_shape, const Shape& out_shape,
+    const TargetMachineFeatures* cpu_features) {
+  if (lhs_shape.element_type() != rhs_shape.element_type() ||
+      lhs_shape.element_type() != out_shape.element_type()) {
+    return false;
+  }
+  if (!IsOneDnnSupportedDType(out_shape.element_type(), cpu_features)) {
     return false;
   }
 
-  TF_ASSIGN_OR_RETURN(DotShape dot_shape, GetDotShape(dot_dimensions, lhs_shape,
-                                                      rhs_shape, out_shape));
+  if (ShapeUtil::IsZeroElementArray(lhs_shape) ||
+      ShapeUtil::IsZeroElementArray(rhs_shape) ||
+      ShapeUtil::IsZeroElementArray(out_shape)) {
+    return false;
+  }
 
-  TF_ASSIGN_OR_RETURN(DotCanonicalDims dot_canonical_dims,
-                      GetDotCanonicalDims(dot_dimensions, dot_shape));
+  // NOLINTNEXTLINE: Use dnnl.hpp for DNNL_MAX_NDIMS for now.
+  if (lhs_shape.dimensions_size() > DNNL_MAX_NDIMS ||
+      rhs_shape.dimensions_size() > DNNL_MAX_NDIMS ||
+      lhs_shape.dimensions_size() != rhs_shape.dimensions_size()) {
+    return false;
+  }
 
-  // Restrict support to no transposes and row-major layouts for now.
-  return dot_canonical_dims.lhs_canonical && dot_canonical_dims.rhs_canonical &&
-         !dot_canonical_dims.lhs_column_major &&
+  auto dot_shape_result =
+      GetDotShape(dot_dimensions, lhs_shape, rhs_shape, out_shape);
+  if (!dot_shape_result.ok()) {
+    VLOG(2) << "GetDotShape Error: " << dot_shape_result.status();
+    return false;
+  }
+  DotShape dot_shape = dot_shape_result.value();
+
+  auto dot_canonical_result = GetDotCanonicalDims(dot_dimensions, dot_shape);
+  if (!dot_canonical_result.ok()) {
+    VLOG(2) << "GetDotCanonicalDims Error: " << dot_canonical_result.status();
+    return false;
+  }
+  DotCanonicalDims dot_canonical_dims = dot_canonical_result.value();
+
+  // Restrict support to row-major layouts.
+  return !dot_canonical_dims.lhs_column_major &&
          !dot_canonical_dims.rhs_column_major;
 }
 