openxla/xla

Minor readability cleanup.
PiperOrigin-RevId: 842450284

xla/service/platform_util.cc
@@ -16,12 +16,14 @@ limitations under the License.
 #include "xla/service/platform_util.h"
 
 #include <cstdlib>
+#include <cstring>
 #include <optional>
 #include <set>
 #include <string>
 #include <vector>
 
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_join.h"
@@ -49,8 +51,9 @@ namespace xla {
 constexpr se::CudaComputeCapability kMinCudaComputeCapability(
     3, 5, se::CudaComputeCapability::FeatureExtension::kNone);
 
-// The name of the interpreter platform.
-constexpr char kInterpreter[] = "interpreter";
+static bool IsInterpreter(const se::Platform* p) {
+  return absl::AsciiStrToLower(p->Name()) == "interpreter";
+}
 
 namespace {
 
@@ -78,14 +81,12 @@ std::string CanonicalPlatformName(absl::string_view platform_name) {
 absl::StatusOr<std::vector<se::Platform*>> GetSupportedPlatforms() {
   return se::PlatformManager::PlatformsWithFilter(
       [](const se::Platform* platform) {
-        auto compiler_status = Compiler::GetForPlatform(platform);
-        bool supported = compiler_status.ok();
-        if (!supported) {
-          LOG(INFO) << "platform " << platform->Name() << " present but no "
-                    << "XLA compiler available: "
-                    << compiler_status.status().message();
+        auto sor = Compiler::GetForPlatform(platform);
+        if (!sor.ok()) {
+          LOG(INFO) << "Platform " << platform->Name() << " present but no "
+                    << "XLA compiler available: " << sor.status().message();
         }
-        return supported;
+        return sor.ok();
       });
 }
 
@@ -103,28 +104,26 @@ PlatformUtil::GetSupportedPlatforms() {
 }
 
 absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
-  const char* maybe_allow_get_default_platform =
-      getenv("XLA_ALLOW_GET_DEFAULT_PLATFORM");
-  if (maybe_allow_get_default_platform != nullptr) {
-    std::string allow_get_default_platform(maybe_allow_get_default_platform);
-    TF_RET_CHECK(allow_get_default_platform == "true")
-        << "GetDefaultPlatform is not allowed (XLA_ALLOW_GET_DEFAULT_PLATFORM="
-        << allow_get_default_platform
-        << ") and the platform must be specified. If this is a test that has "
-           "been migrated to PJRT, double-check that you are using a "
-           "PJRT-compatible test class.";
+  if (const char* allow_default = getenv("XLA_ALLOW_GET_DEFAULT_PLATFORM")) {
+    if (strcmp(allow_default, "true") != 0) {
+      return FailedPrecondition(
+          "GetDefaultPlatform is not allowed "
+          "(XLA_ALLOW_GET_DEFAULT_PLATFORM=\"%s\") and the platform must be "
+          "specified. If this is a test that has been migrated to PJRT, "
+          "double-check that you are using a PJRT-compatible test class.",
+          allow_default);
+    }
   }
   TF_ASSIGN_OR_RETURN(auto platforms, GetSupportedPlatforms());
+  TF_RET_CHECK(!platforms.empty()) << "No platforms found";
 
+  if (platforms.size() == 1) {
+    return platforms[0];
+  }
   se::Platform* platform = nullptr;
-  if (platforms.empty()) {
-    return NotFound("no platforms found");
-  } else if (platforms.size() == 1) {
-    platform = platforms[0];
-  } else if (platforms.size() == 2) {
+  if (platforms.size() == 2) {
     for (int i = 0; i < 2; i++) {
-      if (absl::AsciiStrToLower(platforms[i]->Name()) == kInterpreter &&
-          absl::AsciiStrToLower(platforms[1 - i]->Name()) != kInterpreter) {
+      if (IsInterpreter(platforms[i]) && !IsInterpreter(platforms[1 - i])) {
         platform = platforms[1 - i];
         break;
       }
@@ -139,8 +138,7 @@ absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
       platforms, ", ",
       [](std::string* out, const se::Platform* p) { out->append(p->Name()); });
   return InvalidArgument(
-      "must specify platform because more than one platform (except for the "
-      "interpreter platform) found: %s.",
+      "Plaform must be specified - Multiple platforms found: %s.",
       platforms_string);
 }
 
@@ -155,40 +153,45 @@ absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
 
 // Returns whether the device underlying the given StreamExecutor is supported
 // by XLA.
-static bool IsDeviceSupported(se::StreamExecutor* executor) {
+static absl::Status IsDeviceSupported(se::StreamExecutor* executor) {
   const auto& description = executor->GetDeviceDescription();
   if (executor->GetPlatform()->id() == se::cuda::kCudaPlatformId) {
     // CUDA devices must have a minimum compute capability.
     se::CudaComputeCapability cc = description.cuda_compute_capability();
     if (!cc.SupportsAllFeaturesOf(kMinCudaComputeCapability)) {
-      LOG(INFO) << "StreamExecutor cuda device (" << executor->device_ordinal()
-                << ") is of insufficient compute capability: "
-                << kMinCudaComputeCapability.ToString() << " required, "
-                << "device is " << cc.ToString();
-      return false;
+      return Internal(
+          "StreamExecutor cuda device (%d) requires compute capability '%s' "
+          "but is of '%s'",
+          executor->device_ordinal(), kMinCudaComputeCapability.ToString(),
+          cc.ToString());
     }
   } else if (executor->GetPlatform()->id() == se::rocm::kROCmPlatformId) {
     auto rocm_compute_capability = description.rocm_compute_capability();
     if (!rocm_compute_capability.is_supported_gfx_version()) {
-      LOG(INFO) << "StreamExecutor ROCM device (" << executor->device_ordinal()
-                << ") is of unsupported "
-                << "AMDGPU version : " << rocm_compute_capability.gfx_version()
-                << ". The supported AMDGPU versions are "
-                << rocm_compute_capability.supported_gfx_versions_str() << ".";
-      return false;
+      return Internal(
+          "StreamExecutor ROCM device (%d) is of unsupported AMDGPU version "
+          "'%s' . The supported AMDGPU versions are '%s'.",
+          executor->device_ordinal(), rocm_compute_capability.gfx_version(),
+          rocm_compute_capability.supported_gfx_versions_str());
     }
   }
-  return true;
+  return absl::OkStatus();
 }
 
-absl::StatusOr<std::vector<se::StreamExecutor*>>
-PlatformUtil::GetStreamExecutors(
+static absl::StatusOr<se::StreamExecutor*> ExecutorForDevice(
+    se::Platform* platform, int device_ordinal) {
+  TF_ASSIGN_OR_RETURN(se::StreamExecutor * exec,
+                      platform->ExecutorForDevice(device_ordinal));
+  TF_RETURN_IF_ERROR(IsDeviceSupported(exec));
+  return exec;
+}
+
+static absl::StatusOr<std::vector<int>> GetDeviceOrdinals(
     se::Platform* platform,
     const std::optional<std::set<int>>& allowed_devices) {
   int device_count = platform->VisibleDeviceCount();
-  if (device_count <= 0) {
-    return NotFound("no %s devices found", platform->Name());
-  }
+  TF_RET_CHECK(device_count > 0) << "No devices found on " << platform->Name();
+
   if (platform->id() == se::host::kHostPlatformId) {
     // On host "devices", StreamExecutor exports a device for each hardware
     // thread. Because we parallelize a single computation across threads, it
@@ -199,61 +202,57 @@ PlatformUtil::GetStreamExecutors(
     device_count =
         GetDebugOptionsFromFlags().xla_force_host_platform_device_count();
   }
-  std::vector<se::StreamExecutor*> stream_executors(device_count, nullptr);
-  VLOG(1) << "Initializing devices";
+
+  std::vector<int> device_ordinals;
+  if (allowed_devices.has_value()) {
+    for (int ordinal : *allowed_devices) {
+      TF_RET_CHECK(ordinal >= 0 && ordinal < device_count);
+      device_ordinals.push_back(ordinal);
+    }
+  } else {
+    for (int i = 0; i < device_count; ++i) {
+      device_ordinals.push_back(i);
+    }
+  }
+  return device_ordinals;
+}
+
+absl::StatusOr<std::vector<se::StreamExecutor*>>
+PlatformUtil::GetStreamExecutors(
+    se::Platform* platform,
+    const std::optional<std::set<int>>& allowed_devices) {
+  TF_ASSIGN_OR_RETURN(std::vector<int> device_ordinals,
+                      GetDeviceOrdinals(platform, allowed_devices));
+
+  std::vector<absl::StatusOr<se::StreamExecutor*>> se_sors(
+      device_ordinals.size(),
+      absl::UnknownError("StreamExecutor not initialized"));
   {
-    tsl::thread::ThreadPool thread_pool(tsl::Env::Default(),
-                                        "device_initialization", device_count);
-    auto create_fn = [](se::Platform* platform,
-                        std::vector<se::StreamExecutor*>& stream_executors,
-                        int device_ordinal, int count) {
-      VLOG(1) << "Started device init " << device_ordinal;
-      auto executor_status = platform->ExecutorForDevice(device_ordinal);
-      if (executor_status.ok()) {
-        se::StreamExecutor* executor = executor_status.value();
-        if (IsDeviceSupported(executor)) {
-          stream_executors[count] = executor;
-        }
-      } else {
-        LOG(WARNING) << "unable to create StreamExecutor for "
-                     << platform->Name() << ":" << device_ordinal << ": "
-                     << executor_status.status().message();
-      }
-      VLOG(1) << "Finished device init " << device_ordinal;
-    };
+    tsl::thread::ThreadPool thread_pool(
+        tsl::Env::Default(), "device_initialization", device_ordinals.size());
     // Once a stream executor is instantiated it will cause allocations on
     // the device, for example for GPUs cuda context, cudnn handles etc. will
     // be constructed. By constructing stream executors only on the
     // allowed_devices, we don't make any allocations on other devices.
     // This helps in multi-process executions on the same host like horovod or
     // shared hosts.
-    if (allowed_devices) {
-      int count = 0;
-      for (const auto& i : *allowed_devices) {
-        if (count >= device_count) {
-          break;
-        }
-        thread_pool.Schedule(
-            [platform, &stream_executors, i, count, &create_fn]() {
-              create_fn(platform, stream_executors, i, count);
-            });
-        count++;
-      }
-    } else {
-      for (int i = 0; i < device_count; ++i) {
-        thread_pool.Schedule([platform, &stream_executors, i, &create_fn]() {
-          create_fn(platform, stream_executors, i, i);
-        });
-      }
+    for (int i = 0; i < device_ordinals.size(); ++i) {
+      thread_pool.Schedule([platform, &se_sors, &device_ordinals, i]() {
+        se_sors[i] = ExecutorForDevice(platform, device_ordinals[i]);
+      });
     }
     // Block here in thread_pool destructor until all devices are initialized.
   }
-  VLOG(1) << "Device initialization complete";
 
   std::vector<se::StreamExecutor*> out;
-  for (se::StreamExecutor* executor : stream_executors) {
-    if (executor != nullptr) {
-      out.push_back(executor);
+  for (int i = 0; i < se_sors.size(); ++i) {
+    absl::StatusOr<se::StreamExecutor*>& se_sor = se_sors[i];
+    if (se_sor.ok()) {
+      out.push_back(se_sor.value());
+    } else {
+      LOG(ERROR) << "Failed to create stream executor for device "
+                 << platform->Name() << ":" << device_ordinals[i] << ": "
+                 << se_sor.status();
     }
   }
   if (out.empty()) {

hexagon-geo-surv/tensorflow

third_party/xla/xla/service/platform_util.cc
@@ -16,12 +16,14 @@ limitations under the License.
 #include "xla/service/platform_util.h"
 
 #include <cstdlib>
+#include <cstring>
 #include <optional>
 #include <set>
 #include <string>
 #include <vector>
 
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_join.h"
@@ -45,14 +47,15 @@ limitations under the License.
 
 namespace xla {
 
+namespace {
+
 // Minimum supported CUDA compute capability is 3.5.
 constexpr se::CudaComputeCapability kMinCudaComputeCapability(
     3, 5, se::CudaComputeCapability::FeatureExtension::kNone);
 
-// The name of the interpreter platform.
-constexpr char kInterpreter[] = "interpreter";
-
-namespace {
+bool IsInterpreter(const se::Platform* p) {
+  return absl::AsciiStrToLower(p->Name()) == "interpreter";
+}
 
 std::string CanonicalPlatformName(absl::string_view platform_name) {
   std::string lowercase_platform_name = absl::AsciiStrToLower(platform_name);
@@ -78,15 +81,74 @@ std::string CanonicalPlatformName(absl::string_view platform_name) {
 absl::StatusOr<std::vector<se::Platform*>> GetSupportedPlatforms() {
   return se::PlatformManager::PlatformsWithFilter(
       [](const se::Platform* platform) {
-        auto compiler_status = Compiler::GetForPlatform(platform);
-        bool supported = compiler_status.ok();
-        if (!supported) {
-          LOG(INFO) << "platform " << platform->Name() << " present but no "
-                    << "XLA compiler available: "
-                    << compiler_status.status().message();
-        }
-        return supported;
+        return Compiler::ExistsForPlatform(platform);
       });
+}
+
+// Returns whether the device underlying the given StreamExecutor is supported
+// by XLA.
+absl::Status IsDeviceSupported(se::StreamExecutor* executor) {
+  const auto& description = executor->GetDeviceDescription();
+  if (executor->GetPlatform()->id() == se::cuda::kCudaPlatformId) {
+    // CUDA devices must have a minimum compute capability.
+    se::CudaComputeCapability cc = description.cuda_compute_capability();
+    if (!cc.SupportsAllFeaturesOf(kMinCudaComputeCapability)) {
+      return Internal(
+          "StreamExecutor cuda device (%d) requires compute capability '%s' "
+          "but is of '%s'",
+          executor->device_ordinal(), kMinCudaComputeCapability.ToString(),
+          cc.ToString());
+    }
+  } else if (executor->GetPlatform()->id() == se::rocm::kROCmPlatformId) {
+    auto rocm_compute_capability = description.rocm_compute_capability();
+    if (!rocm_compute_capability.is_supported_gfx_version()) {
+      return Internal(
+          "StreamExecutor ROCM device (%d) is of unsupported AMDGPU version "
+          "'%s' . The supported AMDGPU versions are '%s'.",
+          executor->device_ordinal(), rocm_compute_capability.gfx_version(),
+          rocm_compute_capability.supported_gfx_versions_str());
+    }
+  }
+  return absl::OkStatus();
+}
+
+absl::StatusOr<se::StreamExecutor*> ExecutorForDevice(se::Platform* platform,
+                                                      int device_ordinal) {
+  TF_ASSIGN_OR_RETURN(se::StreamExecutor * exec,
+                      platform->ExecutorForDevice(device_ordinal));
+  TF_RETURN_IF_ERROR(IsDeviceSupported(exec));
+  return exec;
+}
+
+absl::StatusOr<std::vector<int>> GetDeviceOrdinals(
+    se::Platform* platform,
+    const std::optional<std::set<int>>& allowed_devices) {
+  int device_count = platform->VisibleDeviceCount();
+  TF_RET_CHECK(device_count > 0) << "No devices found on " << platform->Name();
+
+  if (platform->id() == se::host::kHostPlatformId) {
+    // On host "devices", StreamExecutor exports a device for each hardware
+    // thread. Because we parallelize a single computation across threads, it
+    // doesn't make sense to expose these as separate devices, so by default we
+    // fix the number of devices to one.  However we do let the user override
+    // this behavior to help run tests on the host that run models in parallel
+    // across multiple devices.
+    device_count =
+        GetDebugOptionsFromFlags().xla_force_host_platform_device_count();
+  }
+
+  std::vector<int> device_ordinals;
+  if (allowed_devices.has_value()) {
+    for (int ordinal : *allowed_devices) {
+      TF_RET_CHECK(ordinal >= 0 && ordinal < device_count);
+      device_ordinals.push_back(ordinal);
+    }
+  } else {
+    for (int i = 0; i < device_count; ++i) {
+      device_ordinals.push_back(i);
+    }
+  }
+  return device_ordinals;
 }
 
 }  // namespace
@@ -103,28 +165,25 @@ PlatformUtil::GetSupportedPlatforms() {
 }
 
 absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
-  const char* maybe_allow_get_default_platform =
-      getenv("XLA_ALLOW_GET_DEFAULT_PLATFORM");
-  if (maybe_allow_get_default_platform != nullptr) {
-    std::string allow_get_default_platform(maybe_allow_get_default_platform);
-    TF_RET_CHECK(allow_get_default_platform == "true")
-        << "GetDefaultPlatform is not allowed (XLA_ALLOW_GET_DEFAULT_PLATFORM="
-        << allow_get_default_platform
-        << ") and the platform must be specified. If this is a test that has "
-           "been migrated to PJRT, double-check that you are using a "
-           "PJRT-compatible test class.";
+  if (const char* allow_default = getenv("XLA_ALLOW_GET_DEFAULT_PLATFORM");
+      allow_default != nullptr && strcmp(allow_default, "true") != 0) {
+    return FailedPrecondition(
+        "GetDefaultPlatform is not allowed "
+        "(XLA_ALLOW_GET_DEFAULT_PLATFORM=\"%s\") and the platform must be "
+        "specified. If this is a test that has been migrated to PJRT, "
+        "double-check that you are using a PJRT-compatible test class.",
+        allow_default);
   }
   TF_ASSIGN_OR_RETURN(auto platforms, GetSupportedPlatforms());
+  TF_RET_CHECK(!platforms.empty()) << "No platforms found";
 
+  if (platforms.size() == 1) {
+    return platforms[0];
+  }
   se::Platform* platform = nullptr;
-  if (platforms.empty()) {
-    return NotFound("no platforms found");
-  } else if (platforms.size() == 1) {
-    platform = platforms[0];
-  } else if (platforms.size() == 2) {
+  if (platforms.size() == 2) {
     for (int i = 0; i < 2; i++) {
-      if (absl::AsciiStrToLower(platforms[i]->Name()) == kInterpreter &&
-          absl::AsciiStrToLower(platforms[1 - i]->Name()) != kInterpreter) {
+      if (IsInterpreter(platforms[i]) && !IsInterpreter(platforms[1 - i])) {
         platform = platforms[1 - i];
         break;
       }
@@ -139,8 +198,7 @@ absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
       platforms, ", ",
       [](std::string* out, const se::Platform* p) { out->append(p->Name()); });
   return InvalidArgument(
-      "must specify platform because more than one platform (except for the "
-      "interpreter platform) found: %s.",
+      "Platform must be specified - Multiple platforms found: %s.",
       platforms_string);
 }
 
@@ -153,107 +211,42 @@ absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
   return platform;
 }
 
-// Returns whether the device underlying the given StreamExecutor is supported
-// by XLA.
-static bool IsDeviceSupported(se::StreamExecutor* executor) {
-  const auto& description = executor->GetDeviceDescription();
-  if (executor->GetPlatform()->id() == se::cuda::kCudaPlatformId) {
-    // CUDA devices must have a minimum compute capability.
-    se::CudaComputeCapability cc = description.cuda_compute_capability();
-    if (!cc.SupportsAllFeaturesOf(kMinCudaComputeCapability)) {
-      LOG(INFO) << "StreamExecutor cuda device (" << executor->device_ordinal()
-                << ") is of insufficient compute capability: "
-                << kMinCudaComputeCapability.ToString() << " required, "
-                << "device is " << cc.ToString();
-      return false;
-    }
-  } else if (executor->GetPlatform()->id() == se::rocm::kROCmPlatformId) {
-    auto rocm_compute_capability = description.rocm_compute_capability();
-    if (!rocm_compute_capability.is_supported_gfx_version()) {
-      LOG(INFO) << "StreamExecutor ROCM device (" << executor->device_ordinal()
-                << ") is of unsupported "
-                << "AMDGPU version : " << rocm_compute_capability.gfx_version()
-                << ". The supported AMDGPU versions are "
-                << rocm_compute_capability.supported_gfx_versions_str() << ".";
-      return false;
-    }
-  }
-  return true;
-}
-
 absl::StatusOr<std::vector<se::StreamExecutor*>>
 PlatformUtil::GetStreamExecutors(
     se::Platform* platform,
     const std::optional<std::set<int>>& allowed_devices) {
-  int device_count = platform->VisibleDeviceCount();
-  if (device_count <= 0) {
-    return NotFound("no %s devices found", platform->Name());
-  }
-  if (platform->id() == se::host::kHostPlatformId) {
-    // On host "devices", StreamExecutor exports a device for each hardware
-    // thread. Because we parallelize a single computation across threads, it
-    // doesn't make sense to expose these as separate devices, so by default we
-    // fix the number of devices to one.  However we do let the user override
-    // this behavior to help run tests on the host that run models in parallel
-    // across multiple devices.
-    device_count =
-        GetDebugOptionsFromFlags().xla_force_host_platform_device_count();
-  }
-  std::vector<se::StreamExecutor*> stream_executors(device_count, nullptr);
-  VLOG(1) << "Initializing devices";
+  TF_ASSIGN_OR_RETURN(std::vector<int> device_ordinals,
+                      GetDeviceOrdinals(platform, allowed_devices));
+
+  std::vector<absl::StatusOr<se::StreamExecutor*>> executors(
+      device_ordinals.size(),
+      absl::UnknownError("StreamExecutor not initialized"));
   {
-    tsl::thread::ThreadPool thread_pool(tsl::Env::Default(),
-                                        "device_initialization", device_count);
-    auto create_fn = [](se::Platform* platform,
-                        std::vector<se::StreamExecutor*>& stream_executors,
-                        int device_ordinal, int count) {
-      VLOG(1) << "Started device init " << device_ordinal;
-      auto executor_status = platform->ExecutorForDevice(device_ordinal);
-      if (executor_status.ok()) {
-        se::StreamExecutor* executor = executor_status.value();
-        if (IsDeviceSupported(executor)) {
-          stream_executors[count] = executor;
-        }
-      } else {
-        LOG(WARNING) << "unable to create StreamExecutor for "
-                     << platform->Name() << ":" << device_ordinal << ": "
-                     << executor_status.status().message();
-      }
-      VLOG(1) << "Finished device init " << device_ordinal;
-    };
+    tsl::thread::ThreadPool thread_pool(
+        tsl::Env::Default(), "device_initialization", device_ordinals.size());
     // Once a stream executor is instantiated it will cause allocations on
     // the device, for example for GPUs cuda context, cudnn handles etc. will
     // be constructed. By constructing stream executors only on the
     // allowed_devices, we don't make any allocations on other devices.
     // This helps in multi-process executions on the same host like horovod or
     // shared hosts.
-    if (allowed_devices) {
-      int count = 0;
-      for (const auto& i : *allowed_devices) {
-        if (count >= device_count) {
-          break;
-        }
-        thread_pool.Schedule(
-            [platform, &stream_executors, i, count, &create_fn]() {
-              create_fn(platform, stream_executors, i, count);
-            });
-        count++;
-      }
-    } else {
-      for (int i = 0; i < device_count; ++i) {
-        thread_pool.Schedule([platform, &stream_executors, i, &create_fn]() {
-          create_fn(platform, stream_executors, i, i);
-        });
-      }
+    for (int i = 0; i < device_ordinals.size(); ++i) {
+      thread_pool.Schedule([platform, &executors, &device_ordinals, i]() {
+        executors[i] = ExecutorForDevice(platform, device_ordinals[i]);
+      });
     }
     // Block here in thread_pool destructor until all devices are initialized.
   }
-  VLOG(1) << "Device initialization complete";
 
   std::vector<se::StreamExecutor*> out;
-  for (se::StreamExecutor* executor : stream_executors) {
-    if (executor != nullptr) {
-      out.push_back(executor);
+  for (int i = 0; i < executors.size(); ++i) {
+    absl::StatusOr<se::StreamExecutor*>& se = executors[i];
+    if (se.ok()) {
+      out.push_back(*se);
+    } else {
+      LOG(ERROR) << "Failed to create stream executor for device "
+                 << platform->Name() << ":" << device_ordinals[i] << ": "
+                 << se.status().message();
     }
   }
   if (out.empty()) {