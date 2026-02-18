google/tsl

Add portable zip library
PiperOrigin-RevId: 871519501

tsl/platform/zip.h
@@ -1,0 +1,59 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_TSL_PLATFORM_ZIP_H_
+#define TENSORFLOW_TSL_PLATFORM_ZIP_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "third_party/protobuf/io/zero_copy_stream.h"
+
+namespace tsl {
+class RandomAccessFile;
+}  // namespace tsl
+
+namespace tsl {
+namespace zip {
+
+class ZipArchive {
+ public:
+  virtual ~ZipArchive() = default;
+
+  // Returns a list of all files/directories in the archive.
+  virtual absl::StatusOr<std::vector<std::string>> GetEntries() = 0;
+
+  // Reads the entire content of a specific entry.
+  virtual absl::StatusOr<std::string> GetContents(absl::string_view entry) = 0;
+
+  // Opens a file in the archive for reading.
+  virtual absl::StatusOr<std::unique_ptr<tsl::RandomAccessFile>> Open(
+      absl::string_view entry) = 0;
+
+  // Returns a ZeroCopyInputStream for reading a specific entry.
+  virtual absl::StatusOr<std::unique_ptr<proto2::io::ZeroCopyInputStream>>
+  GetZeroCopyInputStream(absl::string_view entry) = 0;
+};
+
+// Opens a zip archive from the given path.
+absl::StatusOr<std::unique_ptr<ZipArchive>> Open(absl::string_view path);
+
+}  // namespace zip
+}  // namespace tsl
+
+#endif  // TENSORFLOW_TSL_PLATFORM_ZIP_H_

tsl/platform/zip_test.cc
@@ -1,0 +1,134 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tsl/platform/zip.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "testing/base/public/gmock.h"
+#include "testing/base/public/gunit.h"
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/strings/string_view.h"
+#include "third_party/libzip/lib/zip.h"
+#include "third_party/protobuf/io/zero_copy_stream.h"
+#include "xla/tsl/platform/statusor.h"
+#include "tsl/platform/env.h"
+#include "tsl/platform/file_system.h"
+#include "tsl/platform/path.h"
+
+namespace tsl {
+namespace zip {
+
+using ::absl_testing::IsOk;
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+using ::testing::IsEmpty;
+using ::testing::UnorderedElementsAre;
+
+TEST(ZipTest, OpenNonExistentFile) {
+  EXPECT_THAT(Open("nonexistent.zip"), StatusIs(absl::StatusCode::kInternal));
+}
+
+TEST(ZipTest, ReadArchive) {
+  const std::string zip_path = io::JoinPath(::testing::TempDir(), "test.zip");
+  int error = 0;
+  zip_t* archive_writer =
+      zip_open(zip_path.c_str(), ZIP_CREATE | ZIP_TRUNCATE, &error);
+  ASSERT_NE(archive_writer, nullptr);
+  const std::string a_content = "a content";
+  zip_source_t* s_a = zip_source_buffer(archive_writer, a_content.data(),
+                                        a_content.length(), 0);
+  ASSERT_NE(s_a, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "a.txt", s_a, 0), 0);
+  const std::string c_content = "c content";
+  zip_source_t* s_c = zip_source_buffer(archive_writer, c_content.data(),
+                                        c_content.length(), 0);
+  ASSERT_NE(s_c, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "b/c.txt", s_c, 0), 0);
+  ASSERT_EQ(zip_close(archive_writer), 0);
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+
+  EXPECT_THAT(archive->GetEntries(),
+              IsOkAndHolds(UnorderedElementsAre(
+                  io::JoinPath("/zip", zip_path, "a.txt"),
+                  io::JoinPath("/zip", zip_path, "b/c.txt"))));
+
+  EXPECT_THAT(archive->GetContents("a.txt"), IsOkAndHolds("a content"));
+  EXPECT_THAT(archive->GetContents("b/c.txt"), IsOkAndHolds("c content"));
+  EXPECT_THAT(archive->GetContents("d.txt"),
+              StatusIs(absl::StatusCode::kNotFound));
+
+  TF_ASSERT_OK_AND_ASSIGN(auto raf, archive->Open("a.txt"));
+  absl::string_view read_result;
+  std::vector<char> scratch(9);
+  EXPECT_THAT(raf->Read(0, 9, &read_result, scratch.data()), IsOk());
+  EXPECT_EQ(read_result, "a content");
+
+  EXPECT_THAT(archive->Open("d.txt"), StatusIs(absl::StatusCode::kNotFound));
+}
+
+TEST(ZipTest, ReadArchiveUsingZeroCopyInputStream) {
+  const std::string zip_path =
+      io::JoinPath(::testing::TempDir(), "test_zerocopy.zip");
+  int error = 0;
+  zip_t* archive_writer =
+      zip_open(zip_path.c_str(), ZIP_CREATE | ZIP_TRUNCATE, &error);
+  ASSERT_NE(archive_writer, nullptr);
+  const std::string a_content = "a content";
+  zip_source_t* s_a = zip_source_buffer(archive_writer, a_content.data(),
+                                        a_content.length(), 0);
+  ASSERT_NE(s_a, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "a.txt", s_a, 0), 0);
+  const std::string c_content = "c content";
+  zip_source_t* s_c = zip_source_buffer(archive_writer, c_content.data(),
+                                        c_content.length(), 0);
+  ASSERT_NE(s_c, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "b/c.txt", s_c, 0), 0);
+  ASSERT_EQ(zip_close(archive_writer), 0);
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+
+  TF_ASSERT_OK_AND_ASSIGN(auto stream,
+                          archive->GetZeroCopyInputStream("a.txt"));
+  const void* data;
+  int size;
+  std::string content;
+  while (stream->Next(&data, &size)) {
+    content.append(static_cast<const char*>(data), size);
+  }
+  EXPECT_EQ(content, "a content");
+
+  EXPECT_THAT(archive->GetZeroCopyInputStream("d.txt"),
+              StatusIs(absl::StatusCode::kNotFound));
+}
+
+TEST(ZipTest, EmptyArchive) {
+  const std::string zip_path = io::JoinPath(::testing::TempDir(), "empty.zip");
+  std::unique_ptr<WritableFile> file;
+  ASSERT_OK(Env::Default()->NewWritableFile(zip_path, &file));
+  ASSERT_OK(file->Append(
+      absl::string_view("PK\x05\x06\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 22)));
+  ASSERT_OK(file->Close());
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+  EXPECT_THAT(archive->GetEntries(), IsOkAndHolds(IsEmpty()));
+}
+
+}  // namespace zip
+}  // namespace tsl

hexagon-geo-surv/tensorflow

third_party/xla/xla/tsl/platform/zip.h
@@ -1,0 +1,59 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_TSL_PLATFORM_ZIP_H_
+#define XLA_TSL_PLATFORM_ZIP_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "google/protobuf/io/zero_copy_stream.h"
+
+namespace tsl {
+class RandomAccessFile;
+}  // namespace tsl
+
+namespace tsl {
+namespace zip {
+
+class ZipArchive {
+ public:
+  virtual ~ZipArchive() = default;
+
+  // Returns a list of all files/directories in the archive.
+  virtual absl::StatusOr<std::vector<std::string>> GetEntries() = 0;
+
+  // Reads the entire content of a specific entry.
+  virtual absl::StatusOr<std::string> GetContents(absl::string_view entry) = 0;
+
+  // Opens a file in the archive for reading.
+  virtual absl::StatusOr<std::unique_ptr<tsl::RandomAccessFile>> Open(
+      absl::string_view entry) = 0;
+
+  // Returns a ZeroCopyInputStream for reading a specific entry.
+  virtual absl::StatusOr<std::unique_ptr<google::protobuf::io::ZeroCopyInputStream>>
+  GetZeroCopyInputStream(absl::string_view entry) = 0;
+};
+
+// Opens a zip archive from the given path.
+absl::StatusOr<std::unique_ptr<ZipArchive>> Open(absl::string_view path);
+
+}  // namespace zip
+}  // namespace tsl
+
+#endif  // XLA_TSL_PLATFORM_ZIP_H_

third_party/xla/xla/tsl/platform/zip_test.cc
@@ -1,0 +1,134 @@
+/* Copyright 2026 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/tsl/platform/zip.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/strings/string_view.h"
+#include "third_party/libzip/lib/zip.h"
+#include "google/protobuf/io/zero_copy_stream.h"
+#include "xla/tsl/platform/env.h"
+#include "xla/tsl/platform/file_system.h"
+#include "xla/tsl/platform/statusor.h"
+#include "tsl/platform/path.h"
+
+namespace tsl {
+namespace zip {
+
+using ::absl_testing::IsOk;
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+using ::testing::IsEmpty;
+using ::testing::UnorderedElementsAre;
+
+TEST(ZipTest, OpenNonExistentFile) {
+  EXPECT_THAT(Open("nonexistent.zip"), StatusIs(absl::StatusCode::kInternal));
+}
+
+TEST(ZipTest, ReadArchive) {
+  const std::string zip_path = io::JoinPath(::testing::TempDir(), "test.zip");
+  int error = 0;
+  zip_t* archive_writer =
+      zip_open(zip_path.c_str(), ZIP_CREATE | ZIP_TRUNCATE, &error);
+  ASSERT_NE(archive_writer, nullptr);
+  const std::string a_content = "a content";
+  zip_source_t* s_a = zip_source_buffer(archive_writer, a_content.data(),
+                                        a_content.length(), 0);
+  ASSERT_NE(s_a, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "a.txt", s_a, 0), 0);
+  const std::string c_content = "c content";
+  zip_source_t* s_c = zip_source_buffer(archive_writer, c_content.data(),
+                                        c_content.length(), 0);
+  ASSERT_NE(s_c, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "b/c.txt", s_c, 0), 0);
+  ASSERT_EQ(zip_close(archive_writer), 0);
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+
+  EXPECT_THAT(archive->GetEntries(),
+              IsOkAndHolds(UnorderedElementsAre(
+                  io::JoinPath("/zip", zip_path, "a.txt"),
+                  io::JoinPath("/zip", zip_path, "b/c.txt"))));
+
+  EXPECT_THAT(archive->GetContents("a.txt"), IsOkAndHolds("a content"));
+  EXPECT_THAT(archive->GetContents("b/c.txt"), IsOkAndHolds("c content"));
+  EXPECT_THAT(archive->GetContents("d.txt"),
+              StatusIs(absl::StatusCode::kNotFound));
+
+  TF_ASSERT_OK_AND_ASSIGN(auto raf, archive->Open("a.txt"));
+  absl::string_view read_result;
+  std::vector<char> scratch(9);
+  EXPECT_THAT(raf->Read(0, 9, &read_result, scratch.data()), IsOk());
+  EXPECT_EQ(read_result, "a content");
+
+  EXPECT_THAT(archive->Open("d.txt"), StatusIs(absl::StatusCode::kNotFound));
+}
+
+TEST(ZipTest, ReadArchiveUsingZeroCopyInputStream) {
+  const std::string zip_path =
+      io::JoinPath(::testing::TempDir(), "test_zerocopy.zip");
+  int error = 0;
+  zip_t* archive_writer =
+      zip_open(zip_path.c_str(), ZIP_CREATE | ZIP_TRUNCATE, &error);
+  ASSERT_NE(archive_writer, nullptr);
+  const std::string a_content = "a content";
+  zip_source_t* s_a = zip_source_buffer(archive_writer, a_content.data(),
+                                        a_content.length(), 0);
+  ASSERT_NE(s_a, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "a.txt", s_a, 0), 0);
+  const std::string c_content = "c content";
+  zip_source_t* s_c = zip_source_buffer(archive_writer, c_content.data(),
+                                        c_content.length(), 0);
+  ASSERT_NE(s_c, nullptr);
+  ASSERT_GE(zip_file_add(archive_writer, "b/c.txt", s_c, 0), 0);
+  ASSERT_EQ(zip_close(archive_writer), 0);
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+
+  TF_ASSERT_OK_AND_ASSIGN(auto stream,
+                          archive->GetZeroCopyInputStream("a.txt"));
+  const void* data;
+  int size;
+  std::string content;
+  while (stream->Next(&data, &size)) {
+    content.append(static_cast<const char*>(data), size);
+  }
+  EXPECT_EQ(content, "a content");
+
+  EXPECT_THAT(archive->GetZeroCopyInputStream("d.txt"),
+              StatusIs(absl::StatusCode::kNotFound));
+}
+
+TEST(ZipTest, EmptyArchive) {
+  const std::string zip_path = io::JoinPath(::testing::TempDir(), "empty.zip");
+  std::unique_ptr<WritableFile> file;
+  ASSERT_OK(tsl::Env::Default()->NewWritableFile(zip_path, &file));
+  ASSERT_OK(file->Append(
+      absl::string_view("PK\x05\x06\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 22)));
+  ASSERT_OK(file->Close());
+
+  TF_ASSERT_OK_AND_ASSIGN(auto archive, Open(zip_path));
+  EXPECT_THAT(archive->GetEntries(), IsOkAndHolds(IsEmpty()));
+}
+
+}  // namespace zip
+}  // namespace tsl