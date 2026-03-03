dbcp1/LiteRT

When a weight is cached by the XNNPack delegate, mark original weights as not needed anymore.
PiperOrigin-RevId: 877462933

tflite/delegates/xnnpack/mmap_handle.cc
@@ -14,6 +14,8 @@ limitations under the License.
 ==============================================================================*/
 #include "tflite/delegates/xnnpack/mmap_handle.h"
 
+#include <memory>
+
 #if defined(_WIN32)
 #include <io.h>
 #include <windows.h>
@@ -201,6 +203,37 @@ bool MMapHandle::UnlockMemory() {
 #else
   return munlock(data_, size_ + offset_page_adjustment_) == 0;
 #endif
+}
+
+bool MarkMemoryNotNeeded(void* data, size_t size) {
+  // We don't mark memory chinks that are too small.
+  constexpr const size_t size_threshold = 1024 * 1024;
+#if defined(_WIN32)
+  SYSTEM_INFO sysInfo;
+  GetSystemInfo(&sysInfo);
+  DWORD alignment = sysInfo.dwPageSize;
+#else   // defined(_WIN32)
+  size_t page_size = getpagesize();
+#endif  // defined(_WIN32)
+
+  if (size > size_threshold) {
+    // We align the data buffer to the next page boundary and the size to be a
+    // multiple of the page size.
+    //
+    // - Windows will unlock all pages that contain at least a byte of the given
+    // range, which we want to avoid.
+    // - Linux requires the address to be on a page boundary.
+    void* aligned_data = std::align(page_size, page_size, data, size);
+    size -= size % page_size;
+    if (aligned_data) {
+#if defined(_WIN32)
+      return VirtualUnlock(ptr, space);
+#else  // defined(_WIN32)
+      return madvise(aligned_data, size, MADV_DONTNEED) == 0;
+#endif
+    }
+  }
+  return true;
 }
 
 }  // namespace tflite::xnnpack

hexagon-geo-surv/tensorflow

tensorflow/lite/delegates/xnnpack/mmap_handle.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <memory>
 
 #include "tensorflow/lite/delegates/xnnpack/file_util.h"
 #include "tensorflow/lite/delegates/xnnpack/macros.h"
@@ -201,6 +202,37 @@ bool MMapHandle::UnlockMemory() {
 #else
   return munlock(data_, size_ + offset_page_adjustment_) == 0;
 #endif
+}
+
+bool MarkMemoryNotNeeded(void* data, size_t size) {
+  // We don't mark memory chunks that are too small.
+  constexpr const size_t size_threshold = 1024 * 1024;
+#if defined(_WIN32)
+  SYSTEM_INFO sysInfo;
+  GetSystemInfo(&sysInfo);
+  DWORD page_size = sysInfo.dwPageSize;
+#else
+  size_t page_size = getpagesize();
+#endif
+
+  if (size > size_threshold) {
+    // We align the data buffer to the next page boundary and the size to be a
+    // multiple of the page size.
+    //
+    // - Windows will unlock all pages that contain at least a byte of the given
+    // range, which we want to avoid.
+    // - Linux requires the address to be on a page boundary.
+    void* aligned_data = std::align(page_size, page_size, data, size);
+    size -= size % page_size;
+    if (aligned_data) {
+#if defined(_WIN32)
+      return VirtualUnlock(aligned_data, size);
+#else
+      return madvise(aligned_data, size, MADV_DONTNEED) == 0;
+#endif
+    }
+  }
+  return true;
 }
 
 }  // namespace tflite::xnnpack