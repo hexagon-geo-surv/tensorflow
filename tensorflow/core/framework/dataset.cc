8ad813b994b5c82653aa82e12ac2705001622ee6
Ilya Tikhonovskiy <loislo@google.com>
2026-03-11 23:22:03 -0700

    Add specific deprecated overloads for tsl::errors::DataLoss and others that would match the automatic replacement requirements.
    PiperOrigin-RevId: 882400734

--- tensorflow/core/framework/dataset.cc
+++ tensorflow/core/framework/dataset.cc
@@ -1132,6 +1132,13 @@ std::string DatasetBaseIterator::BuildTraceMeName() {
   return result;
 }
 
+// A helper function to create an internal error without materializing arguments
+// for StrCat. This is to avoid the stack overflow for the cases when it is
+// executed from python.
+inline const absl::AlphaNum& PrepareForStrCat(const absl::AlphaNum& a) {
+  return a;
+}
+
 absl::Status DatasetBaseIterator::GetNext(IteratorContext* ctx,
                                           std::vector<Tensor>* out_tensors,
                                           bool* end_of_sequence) {
@@ -1160,17 +1167,18 @@ absl::Status DatasetBaseIterator::GetNext(IteratorContext* ctx,
   ctx->SaveCheckpoint(this);
   if (!SymbolicCheckpointCompatible()) {
     ctx->UpdateCheckpointStatus([this]() {
-      return errors::Unimplemented(dataset()->type_string(),
-                                   " does not support symbolic checkpointing.");
+      return errors::UnimplementedError(
+          dataset()->type_string(),
+          " does not support symbolic checkpointing.");
     });
   }
   if (TF_PREDICT_TRUE(s.ok())) {
     if (TF_PREDICT_TRUE(!*end_of_sequence)) {
       if (TF_PREDICT_FALSE(out_tensors->size() !=
                            dataset()->output_dtypes().size())) {
-        return errors::Internal("Expected ", dataset()->output_dtypes().size(),
-                                " components but got ", out_tensors->size(),
-                                ".");
+        return errors::InternalError(
+            "Expected ", dataset()->output_dtypes().size(),
+            " components but got ", out_tensors->size(), ".");
       }
       RecordElement(ctx, out_tensors);
     } else {
@@ -1185,11 +1193,12 @@ absl::Status DatasetBaseIterator::GetNext(IteratorContext* ctx,
     }
   }
   if (TF_PREDICT_FALSE(absl::IsOutOfRange(s))) {
-    s = errors::Internal("Iterator \"", params_.prefix,
-                         "\" returned `OutOfRange`. This indicates an "
-                         "implementation error as `OutOfRange` errors are not "
-                         "expected to be returned here. Original message: ",
-                         s.message());
+    s = errors::InternalError(
+        "Iterator \"", params_.prefix,
+        "\" returned `OutOfRange`. This indicates an "
+        "implementation error as `OutOfRange` errors are not "
+        "expected to be returned here. Original message: ",
+        s.message());
     LOG(ERROR) << s;
   }
   DVLOG(3) << prefix() << " GetNext exit";
