e72c6444454791bb1245b726c0d8214b6254998f
Matthias Guenther <mrguenther@google.com>
2026-04-30 17:48:04 -0700

    Update `ml-build` Dockerfile to support StableHLO CI workflows
    PiperOrigin-RevId: 908471636

--- ci/official/containers/ml_build/setup.toolchain.non_hermetic.sh
+++ ci/official/containers/ml_build/setup.toolchain.non_hermetic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,18 +15,14 @@
 # limitations under the License.
 # ==============================================================================
 #
-# setup.packages.sh: Given a list of Ubuntu packages, install them and clean up.
-# Usage: setup.packages.sh <package_list.txt>
-set -e
+# Sets up custom apt sources for our TF images.
 
 # Prevent apt install tzinfo from asking our location (assumes UTC)
 export DEBIAN_FRONTEND=noninteractive
 
-# DO_NOT_SUBMIT: Retry apt-get update up to 5 times on failure
-for i in {1..5}; do
-  apt-get update && break || { echo "apt-get update failed, retrying in 5s..."; sleep 5; }
-done
-apt-get upgrade -y
-# Remove commented lines and blank lines
-apt-get install -y --no-install-recommends $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' "$1" | sort -u)
-rm -rf /var/lib/apt/lists/*
+# Install `software-properties-common` for `add-apt-repository`.
+apt-get update
+apt-get install -y software-properties-common
+
+# Add Ubuntu Toolchain PPA for newer GCC/libstdc++.
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
