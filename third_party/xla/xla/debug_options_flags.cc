a727d594deada1abddb23f8dc96b010b2fcf2f6e
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-18 15:07:39 -0700

    Reverts e1dd4d4231ee75f6590ccc68432f1c67e2a0d013
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/45891 from ezhulenev:se-d-addr-get-slice e7032fa1a2345f0c688d597420aa1ddbb29747db
    PiperOrigin-RevId: 950165601

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -392,7 +392,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_experimental_enable_fusion_block_level_rewriter(false);
 
-  opts.set_xla_gpu_default_to_alg_dot_bf16_bf16_f32(false);
+  opts.set_xla_gpu_match_tpu_precision(false);
   opts.set_xla_gpu_enable_libnvptxcompiler(
       stream_executor::IsLibNvPtxCompilerSupported());
   opts.set_xla_gpu_libnvjitlink_mode(DebugOptions::LIB_NV_JIT_LINK_MODE_AUTO);
@@ -496,7 +496,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_early_exit_with_layouts(false);
   opts.set_xla_gpu_experimental_all_fusions_with_triton(false);
   opts.set_xla_gpu_experimental_ragged_all_to_all_use_barrier(true);
-  opts.set_xla_gpu_experimental_ragged_all_to_all_use_barrier_with_nccl(true);
   opts.set_xla_gpu_ragged_all_to_all_mode(
       DebugOptions::COLLECTIVES_PRIVATE_MEMORY);
   opts.set_xla_gpu_experimental_use_ragged_dot_grouped_gemm(true);
@@ -1877,14 +1876,17 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       debug_options->xla_gpu_force_compilation_parallelism(),
       "Overrides normal multi-threaded compilation setting to use this many "
       "threads. Setting to 0 (the default value) means no enforcement."));
-
+  flag_list->push_back(
+      tsl::Flag("xla_gpu_default_to_alg_dot_bf16_bf16_f32",
+                bool_setter_for(&DebugOptions::set_xla_gpu_match_tpu_precision),
+                debug_options->xla_gpu_match_tpu_precision(),
+                "Deprecated. Use `xla_gpu_match_tpu_precision` instead."));
   flag_list->push_back(tsl::Flag(
-      "xla_gpu_default_to_alg_dot_bf16_bf16_f32",
-      bool_setter_for(
-          &DebugOptions::set_xla_gpu_default_to_alg_dot_bf16_bf16_f32),
-      debug_options->xla_gpu_default_to_alg_dot_bf16_bf16_f32(),
+      "xla_gpu_match_tpu_precision",
+      bool_setter_for(&DebugOptions::set_xla_gpu_match_tpu_precision),
+      debug_options->xla_gpu_match_tpu_precision(),
       "Use the dot precision algorithm `ALG_DOT_BF16_BF16_F32 by default for "
-      "f32 dots."));
+      "f32 dots. This leads to the same precision as on TPU."));
   flag_list->push_back(
       tsl::Flag("xla_gpu_deterministic_ops",
                 bool_setter_for(&DebugOptions::set_xla_gpu_deterministic_ops),
